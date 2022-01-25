17299fee56df3adad5717c5eb446fde71210a6cf
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-01-25 17:19:12 +0900

    retrive snippet_placeholder

--- .config/nvim/after/plugin/cmp.lua
+++ .config/nvim/after/plugin/cmp.lua
@@ -136,6 +136,6 @@ if status_lspkind then
         max_num_results = 5;
         sort = true;
         run_on_every_keystroke = true;
-	      snippet_placeholder = '..';
+	    snippet_placeholder = '..';
       })
   end
