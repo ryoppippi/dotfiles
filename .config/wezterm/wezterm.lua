b7d333a9329a3620dff7697acbfcac23f646fef8
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-04-17 11:01:46 +0200

    update wezterm
    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,14 +3,14 @@ local wezterm = require 'wezterm';
 
 return {
   use_ime = true,
-  font = wezterm.font("UDEV Gothic 35NFLG"),
+  font = wezterm.font("UDEV Gothic 35LG"),
   font_size = 17.0,
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
   colors = {
     background = '#1e2127',
     foreground = '#e6efff',
-    ansi = {"#1e2127","#e06c75", "#98c379", "#d19a66", "#61afef", "#c678dd", "#56b6c2", "#828791"},
-    brights = {"#5c6370", "#e06c75", "#98c379", "#d19a66", "#61afef", "#c678dd", "#56b6c2", "#e6efff"},
+    ansi = { "#1e2127", "#e06c75", "#98c379", "#d19a66", "#61afef", "#c678dd", "#56b6c2", "#828791" },
+    brights = { "#5c6370", "#e06c75", "#98c379", "#d19a66", "#61afef", "#c678dd", "#56b6c2", "#e6efff" },
   },
 }
