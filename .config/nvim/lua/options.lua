2d5e7e8d9c467b2762cd3ac1148c01dab6695053
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-04-18 11:09:12 +0100

    change tab width

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -39,9 +39,9 @@ vim.opt.showcmd = true
 vim.opt.termguicolors = true
 
 vim.opt.fenc = "utf-8"
--- vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+-- vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
