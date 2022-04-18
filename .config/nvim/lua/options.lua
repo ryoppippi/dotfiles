621c6ddf21f62cb5e0d5321aebe088528dd3d577
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-04-18 14:06:50 +0100

    fix variables

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,4 +1,6 @@
 -- setting
+vim.g.do_filetype_lua = 1
+vim.g.did_load_filetypes = 0
 vim.g.did_install_default_menus = 1
 vim.g.did_install_syntax_menu = 1
 vim.g.did_indent_on = 1
@@ -16,9 +18,6 @@ vim.g.loaded_tarPlugin = 1
 vim.g.loaded_tutor_mode_plugin = 1
 vim.g.loaded_zipPlugin = 1
 vim.g.skip_loading_mswin = 1
-
-vim.g.did_load_filetypes = 1
-vim.g.do_filetype_lua = 1
 
 vim.opt.backup = true
 vim.opt.backupdir = os.getenv("HOME") .. "/.vim_backup"
@@ -39,9 +38,10 @@ vim.opt.showcmd = true
 vim.opt.termguicolors = true
 
 vim.opt.fenc = "utf-8"
--- vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+local tabwidth = 2
+vim.opt.tabstop = tabwidth
+vim.opt.softtabstop = tabwidth
+vim.opt.shiftwidth = tabwidth
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
