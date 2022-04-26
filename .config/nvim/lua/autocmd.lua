be31d123cc23e6b5199db2eae3c20a8209d6eef4
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-04-26 17:03:46 +0100

    fix terminal

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -3,3 +3,8 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 	command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]],
 	group = vimInit,
 })
+
+vim.api.nvim_create_autocmd("TermOpen", {
+	pattern = "*",
+	command = "startinsert",
+})
