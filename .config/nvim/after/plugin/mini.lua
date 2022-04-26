c1421b6926965dadbbced1fe445dcdc8e075e97a
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-04-26 11:20:00 +0100

    decrease delay

--- .config/nvim/after/plugin/mini.lua
+++ .config/nvim/after/plugin/mini.lua
@@ -28,7 +28,7 @@ function load()
 
 		-- Delay (in ms) between jump and highlighting all possible jumps. Set to
 		-- a very big number (like 10^7) to virtually disable highlighting.
-		highlight_delay = 250,
+		highlight_delay = 100,
 	})
 end
 
