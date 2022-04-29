c301260b07ef8e5e5c9e09a3e445f6ed591bcfa9
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-04-29 22:13:29 +0100

    format

--- .config/nvim/after/plugin/mini.lua
+++ .config/nvim/after/plugin/mini.lua
@@ -6,30 +6,28 @@ function load()
   -- cursorword.setup()
 
   local status, indent = pcall(require, "mini.indentscope")
-  if not status then
-    return
+  if status then
+    indent.setup()
   end
-  indent.setup()
 
   local statusJ, jump = pcall(require, "mini.jump")
-  if not statusJ then
-    return
-  end
-  jump.setup({
-    mappings = {
-      forward = "f",
-      backward = "F",
-      -- forward_till = "t",
-      forward_till = "",
-      -- backward_till = "T",
-      backward_till = "",
-      repeat_jump = "",
-    },
+  if statusJ then
+    jump.setup({
+      mappings = {
+        forward = "f",
+        backward = "F",
+        -- forward_till = "t",
+        forward_till = "",
+        -- backward_till = "T",
+        backward_till = "",
+        repeat_jump = "",
+      },
 
-    -- Delay (in ms) between jump and highlighting all possible jumps. Set to
-    -- a very big number (like 10^7) to virtually disable highlighting.
-    highlight_delay = 100,
-  })
+      -- Delay (in ms) between jump and highlighting all possible jumps. Set to
+      -- a very big number (like 10^7) to virtually disable highlighting.
+      highlight_delay = 100,
+    })
+  end
 end
 
 vim.api.nvim_create_autocmd("VimEnter", {
