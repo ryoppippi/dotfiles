85ff31932eec565fb03e3641b71d7aef66febdd4
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-05-17 13:55:30 +0100

    update

--- .config/nvim/lua/plugin/config/feline.lua
+++ .config/nvim/lua/plugin/config/feline.lua
@@ -14,11 +14,6 @@ local function loading()
     bufnames = {},
   }
 
-  local components = {
-    active = { {}, {}, {} },
-    inactive = { {}, {}, {} },
-  }
-
   local colors = {
     bg = "#282828",
     black = "#282828",
@@ -96,327 +91,347 @@ local function loading()
     "terminal",
   }
 
-  -- LEFT
+  local comps = {
+    vi_mode = {
+      provider = function()
+        local mode = vi_mode_utils.get_vim_mode()
+        return " " .. mode .. " "
+      end,
+      short_provider = function()
+        local mode = vi_mode_utils.get_vim_mode()
+        return " " .. string.sub(mode, 1, 1) .. " "
+      end,
+      hl = function()
+        local val = {}
+        val.bg = vi_mode_utils.get_mode_color()
+        val.fg = "black"
+        val.style = "bold"
+        return val
+      end,
+      right_sep = " ",
+    },
 
-  -- vi-mode
-  components.active[1][1] = {
-    provider = function()
-      local mode = vi_mode_utils.get_vim_mode()
-      return " " .. mode .. " "
-    end,
-    short_provider = function()
-      local mode = vi_mode_utils.get_vim_mode()
-      return " " .. string.sub(mode, 1, 1) .. " "
-    end,
-    hl = function()
-      local val = {}
-      val.bg = vi_mode_utils.get_mode_color()
-      val.fg = "black"
-      val.style = "bold"
-      return val
-    end,
-    right_sep = " ",
-  }
-  -- filename
-  components.active[1][2] = {
-    provider = {
-      name = "file_info",
-      opts = {
-        type = "relative",
+    file_name = {
+      provider = {
+        name = "file_info",
+        opts = {
+          type = "relative",
+        },
       },
-    },
-    short_provider = {
-      name = "file_info",
-      opts = {
-        type = "base-only",
+      short_provider = {
+        name = "file_info",
+        opts = {
+          type = "relative-short",
+        },
       },
-    },
-    hl = {
-      fg = "white",
-      bg = "bg",
-      style = "bold",
-    },
-    icon = "",
-  }
-
-  -- nvimGps
-  components.active[1][3] = {
-    provider = function()
-      return gps ~= nil and gps.get_location()
-    end,
-    short_provider = function()
-      return ""
-    end,
-    enabled = function()
-      return gps ~= nil and gps.is_available()
-    end,
-    hl = {
-      fg = "white",
-      bg = "bg",
-      style = "bold",
-    },
-    left_sep = {
-      str = " > ",
       hl = {
         fg = "white",
         bg = "bg",
         style = "bold",
       },
+      icon = "",
+      priority = -20,
     },
-  }
 
-  -- MID
-
-  -- gitBranch
-  components.active[2][1] = {
-    provider = "git_branch",
-    short_provider = "",
-    hl = {
-      fg = "yellow",
-      bg = "bg",
-      style = "bold",
-    },
-  }
-  -- diffAdd
-  components.active[2][2] = {
-    provider = "git_diff_added",
-    hl = {
-      fg = "green",
-      bg = "bg",
-      style = "bold",
-    },
-  }
-  -- diffModfified
-  components.active[2][3] = {
-    provider = "git_diff_changed",
-    hl = {
-      fg = "orange",
-      bg = "bg",
-      style = "bold",
-    },
-  }
-  -- diffRemove
-  components.active[2][4] = {
-    provider = "git_diff_removed",
-    hl = {
-      fg = "red",
-      bg = "bg",
-      style = "bold",
-    },
-  }
-  -- diagnosticErrors
-  components.active[2][5] = {
-    provider = "diagnostic_errors",
-    enabled = function()
-      return lsp.diagnostics_exist(vim.diagnostic.severity.ERROR)
-    end,
-    hl = {
-      fg = "red",
-      style = "bold",
-    },
-  }
-  -- diagnosticWarn
-  components.active[2][6] = {
-    provider = "diagnostic_warnings",
-    enabled = function()
-      return lsp.diagnostics_exist(vim.diagnostic.severity.WARN)
-    end,
-    hl = {
-      fg = "yellow",
-      style = "bold",
-    },
-  }
-  -- diagnosticHint
-  components.active[2][7] = {
-    provider = "diagnostic_hints",
-    enabled = function()
-      return lsp.diagnostics_exist(vim.diagnostic.severity.HINT)
-    end,
-    hl = {
-      fg = "cyan",
-      style = "bold",
-    },
-  }
-  -- diagnosticInfo
-  components.active[2][8] = {
-    provider = "diagnostic_info",
-    enabled = function()
-      return lsp.diagnostics_exist(vim.diagnostic.severity.INFO)
-    end,
-    hl = {
-      fg = "skyblue",
-      style = "bold",
-    },
-  }
-
-  -- RIGHT
-
-  -- fileIcon
-  components.active[3][1] = {
-    provider = function()
-      local filename = vim.fn.expand("%:t")
-      local extension = vim.fn.expand("%:e")
-      local icon = require("nvim-web-devicons").get_icon(filename, extension)
-      if icon == nil then
-        icon = ""
-      end
-      return icon
-    end,
-    hl = function()
-      local val = {}
-      local filename = vim.fn.expand("%:t")
-      local extension = vim.fn.expand("%:e")
-      local icon, name = require("nvim-web-devicons").get_icon(filename, extension)
-      if icon ~= nil then
-        val.fg = vim.fn.synIDattr(vim.fn.hlID(name), "fg")
-      else
-        val.fg = "white"
-      end
-      val.bg = "bg"
-      val.style = "bold"
-      return val
-    end,
-    right_sep = " ",
-  }
-  -- fileType
-  components.active[3][2] = {
-    provider = "file_type",
-    short_provider = "",
-    hl = function()
-      local val = {}
-      local filename = vim.fn.expand("%:t")
-      local extension = vim.fn.expand("%:e")
-      local icon, name = require("nvim-web-devicons").get_icon(filename, extension)
-      if icon ~= nil then
-        val.fg = vim.fn.synIDattr(vim.fn.hlID(name), "fg")
-      else
-        val.fg = "white"
-      end
-      val.bg = "bg"
-      val.style = "bold"
-      return val
-    end,
-    right_sep = " ",
-  }
-  -- fileSize
-  components.active[3][3] = {
-    provider = "file_size",
-    enabled = function()
-      return vim.fn.getfsize(vim.fn.expand("%:t")) > 0
-    end,
-    hl = {
-      fg = "skyblue",
-      bg = "bg",
-      style = "bold",
-    },
-    right_sep = " ",
-  }
-  -- fileFormat
-  components.active[3][4] = {
-    provider = function()
-      local icon, os = get_file_icon()
-      return icon .. os
-    end,
-    short_provider = function()
-      local icon, _ = get_file_icon()
-      return icon
-    end,
-    hl = {
-      fg = "white",
-      bg = "bg",
-      style = "bold",
-    },
-    right_sep = " ",
-  }
-
-  -- fileEncode
-  components.active[3][5] = {
-    provider = "file_encoding",
-    short_provider = "",
-    hl = {
-      fg = "white",
-      bg = "bg",
-      style = "bold",
-    },
-    right_sep = " ",
-  }
-  -- lineInfo
-  components.active[3][6] = {
-    provider = "position",
-    short_provider = "",
-    hl = {
-      fg = "white",
-      bg = "bg",
-      style = "bold",
-    },
-    right_sep = " ",
-  }
-  -- linePercent
-  components.active[3][7] = {
-    provider = "line_percentage",
-    hl = {
-      fg = "white",
-      bg = "bg",
-      style = "bold",
-    },
-    right_sep = " ",
-  }
-  -- scrollBar
-  components.active[3][8] = {
-    provider = "scroll_bar",
-    short_provider = "",
-    hl = {
-      fg = "yellow",
-      bg = "bg",
-    },
-  }
-
-  -- INACTIVE
-
-  -- fileType
-  components.inactive[1][1] = {
-    provider = "file_type",
-    hl = {
-      fg = "black",
-      bg = "cyan",
-      style = "bold",
-    },
-    left_sep = {
-      str = " ",
+    nvim_gps = {
+      provider = function()
+        return gps.get_location({ depth = 0 })
+      end,
+      enabled = function()
+        return gps ~= nil and gps.is_available()
+      end,
       hl = {
-        fg = "NONE",
-        bg = "cyan",
+        fg = "white",
+        bg = "bg",
+        style = "bold",
       },
-    },
-    right_sep = {
-      {
-        str = " ",
+      left_sep = {
+        str = " > ",
         hl = {
-          fg = "NONE",
-          bg = "cyan",
+          fg = "white",
+          bg = "bg",
+          style = "bold",
         },
       },
-      " ",
+      priority = -50,
+      truncate_hide = true,
+    },
+
+    --mid
+    -- gitBranch
+    git_branch = {
+      provider = "git_branch",
+      hl = {
+        fg = "yellow",
+        bg = "bg",
+        style = "bold",
+      },
+      truncate_hide = true,
+    },
+    -- diffAdd
+    git_diff_added = {
+      provider = "git_diff_added",
+      hl = {
+        fg = "green",
+        bg = "bg",
+        style = "bold",
+      },
+    },
+    -- diffModfified
+    git_diff_changed = {
+      provider = "git_diff_changed",
+      hl = {
+        fg = "orange",
+        bg = "bg",
+        style = "bold",
+      },
+      icon = {
+        str = " ~",
+        -- str = "  ",
+      },
+    },
+    -- diffRemove
+    git_diff_removed = {
+      provider = "git_diff_removed",
+      hl = {
+        fg = "red",
+        bg = "bg",
+        style = "bold",
+      },
+    },
+    -- diagnosticErrors
+    lsp_diagnostic_errors = {
+      provider = "diagnostic_errors",
+      enabled = function()
+        return lsp.diagnostics_exist(vim.diagnostic.severity.ERROR)
+      end,
+      hl = {
+        fg = "red",
+        style = "bold",
+      },
+    },
+    -- diagnosticWarn
+    lsp_diagnostic_warnings = {
+      provider = "diagnostic_warnings",
+      enabled = function()
+        return lsp.diagnostics_exist(vim.diagnostic.severity.WARN)
+      end,
+      hl = {
+        fg = "yellow",
+        style = "bold",
+      },
+    },
+    -- diagnosticHint
+    lsp_diagnostic_hints = {
+      provider = "diagnostic_hints",
+      enabled = function()
+        return lsp.diagnostics_exist(vim.diagnostic.severity.HINT)
+      end,
+      hl = {
+        fg = "cyan",
+        style = "bold",
+      },
+    },
+
+    -- diagnosticInfo
+    lsp_diagnostic_info = {
+      provider = "diagnostic_info",
+      enabled = function()
+        return lsp.diagnostics_exist(vim.diagnostic.severity.INFO)
+      end,
+      hl = {
+        fg = "skyblue",
+        style = "bold",
+      },
+    },
+
+    -- RIGHT
+
+    -- fileIcon
+    file_icon = {
+      provider = function()
+        local filename = vim.fn.expand("%:t")
+        local extension = vim.fn.expand("%:e")
+        local icon = require("nvim-web-devicons").get_icon(filename, extension)
+        if icon == nil then
+          icon = ""
+        end
+        return icon
+      end,
+      hl = function()
+        local val = {}
+        local filename = vim.fn.expand("%:t")
+        local extension = vim.fn.expand("%:e")
+        local icon, name = require("nvim-web-devicons").get_icon(filename, extension)
+        if icon ~= nil then
+          val.fg = vim.fn.synIDattr(vim.fn.hlID(name), "fg")
+        else
+          val.fg = "white"
+        end
+        val.bg = "bg"
+        val.style = "bold"
+        return val
+      end,
+      right_sep = " ",
+    },
+    -- fileType
+    file_type = {
+      provider = "file_type",
+      hl = function()
+        local val = {}
+        local filename = vim.fn.expand("%:t")
+        local extension = vim.fn.expand("%:e")
+        local icon, name = require("nvim-web-devicons").get_icon(filename, extension)
+        if icon ~= nil then
+          val.fg = vim.fn.synIDattr(vim.fn.hlID(name), "fg")
+        else
+          val.fg = "white"
+        end
+        val.bg = "bg"
+        val.style = "bold"
+        return val
+      end,
+      right_sep = " ",
+      truncate_hide = true,
+    },
+
+    -- fileSize
+    file_size = {
+      provider = "file_size",
+      enabled = function()
+        return vim.fn.getfsize(vim.fn.expand("%:t")) > 0
+      end,
+      hl = {
+        fg = "skyblue",
+        bg = "bg",
+        style = "bold",
+      },
+      right_sep = " ",
+      truncate_hide = true,
+    },
+
+    -- fileFormat
+    file_format = {
+      provider = function()
+        local icon, os = get_file_icon()
+        return icon .. os
+      end,
+      short_provider = function()
+        local icon, _ = get_file_icon()
+        return icon
+      end,
+      hl = {
+        fg = "white",
+        bg = "bg",
+        style = "bold",
+      },
+      right_sep = " ",
+      priority = -20,
+      truncate_hide = true,
+    },
+
+    -- fileEncode
+    file_encode = {
+      provider = "file_encoding",
+      hl = {
+        fg = "white",
+        bg = "bg",
+        style = "bold",
+      },
+      right_sep = " ",
+      priority = -10,
+      truncate_hide = true,
+    },
+
+    -- lineInfo
+    line_info = {
+      provider = "position",
+      hl = {
+        fg = "white",
+        bg = "bg",
+        style = "bold",
+      },
+      right_sep = " ",
+      padding = true,
+      truncate_hide = true,
+    },
+
+    -- linePercent
+    line_percent = {
+      provider = "line_percentage",
+      hl = {
+        fg = "white",
+        bg = "bg",
+        style = "bold",
+      },
+      right_sep = " ",
+    },
+
+    -- scrollBar
+    scroll_bar = {
+      provider = "scroll_bar",
+      hl = {
+        fg = "yellow",
+        bg = "bg",
+      },
+      priority = -10,
+      truncate_hide = true,
+    },
+
+    -- LspName
+    lsp_client_name = {
+      provider = "lsp_client_names",
+      enabled = function()
+        return checkwidth(110)
+      end,
+      hl = {
+        fg = "yellow",
+        bg = "bg",
+        style = "bold",
+      },
+      right_sep = " ",
+      priority = -50,
+      truncate_hide = true,
     },
   }
-  -- LspName
-  components.inactive[3][2] = {
-    provider = "lsp_client_names",
-    enabled = function()
-      return checkwidth(110)
-    end,
-    hl = {
-      fg = "yellow",
-      bg = "bg",
-      style = "bold",
+  local components = {
+    active = {
+      -- left
+      {
+        comps.vi_mode,
+        comps.file_name,
+        comps.nvim_gps,
+      },
+      -- middle
+      {
+        comps.git_branch,
+        comps.git_diff_added,
+        comps.git_diff_changed,
+        comps.git_diff_removed,
+        comps.lsp_diagnostic_errors,
+        comps.lsp_diagnostic_warnings,
+        comps.lsp_diagnostic_hints,
+        comps.lsp_diagnostic_info,
+      },
+      -- right
+      {
+        comps.lsp_client_name,
+        comps.file_icon,
+        comps.file_type,
+        comps.file_size,
+        comps.file_format,
+        comps.file_encode,
+        comps.line_info,
+        comps.scroll_bar,
+      },
     },
-    right_sep = " ",
+    inactive = { {}, {}, {} },
   }
 
   require("feline").setup({
     theme = colors,
-    -- default_bg = bg,
-    -- default_fg = fg,
+    default_bg = colors.bg,
+    default_fg = colors.fg,
     vi_mode_colors = vi_mode_colors,
     components = components,
     force_inactive = force_inactive,
