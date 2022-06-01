e0faf75151a2bc930635593d6cfc11f3c85eed18
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-06-01 20:14:41 +0100

    add yanky and colortils

---
+++ .config/nvim/lua/plugin/config/yanky.lua
@@ -0,0 +1,20 @@
+local plugin_name = "yanky"
+if not require("utils.plugin").is_exists(plugin_name) then
+  return
+end
+
+local function keymap()
+  vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)", {})
+  vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)", {})
+  vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)", {})
+  vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)", {})
+  vim.keymap.set("n", "<c-m>", "<Plug>(YankyCycleForward)", {})
+  vim.keymap.set("n", "<c-p>", "<Plug>(YankyCycleBackward)", {})
+end
+
+local function loading()
+  keymap()
+  require(plugin_name).setup({})
+end
+
+require("utils.plugin").force_load_on_event(plugin_name, loading)
