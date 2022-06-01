033447a7827b70fb231cacfc6b3ff1abe7bb918c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-06-01 20:16:55 +0100

    fix keymap

--- .config/nvim/lua/plugin/config/substitute.lua
+++ .config/nvim/lua/plugin/config/substitute.lua
@@ -3,13 +3,20 @@ if not require("utils.plugin").is_exists(plugin_name) then
   return
 end
 
+local function keymap()
+  vim.keymap.set("n", "cx", "<cmd>lua require('substitute').operator()<cr>", { noremap = true })
+  vim.keymap.set("x", "cx", "<cmd>lua require('substitute').visual()<cr>", { noremap = true })
+  vim.keymap.set("n", "cxc", "<cmd>lua require('substitute').line()<cr>", { noremap = true })
+  vim.keymap.set("n", "Cx", "<cmd>lua require('substitute').eol()<cr>", { noremap = true })
+end
+
 local function loading()
-  require(plugin_name).setup()
+  keymap()
+  require(plugin_name).setup({
+    on_substitute = function(event)
+      require("yanky").init_ring("p", event.register, event.count, event.vmode:match("[vV�]"))
+    end,
+  })
 end
 
 require("utils.plugin").force_load_on_event(plugin_name, loading)
-
-vim.keymap.set("n", "cx", "<cmd>lua require('substitute').operator()<cr>", { noremap = true })
-vim.keymap.set("x", "cx", "<cmd>lua require('substitute').visual()<cr>", { noremap = true })
-vim.keymap.set("n", "cxc", "<cmd>lua require('substitute').line()<cr>", { noremap = true })
-vim.keymap.set("n", "cX", "<cmd>lua require('substitute').eol()<cr>", { noremap = true })
