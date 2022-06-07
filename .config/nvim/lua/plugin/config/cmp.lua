0ae9fdde8328608e8d64e56a7d171db47638567c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-06-07 09:47:44 +0100

    fix cmp configs

--- .config/nvim/lua/plugin/config/cmp.lua
+++ .config/nvim/lua/plugin/config/cmp.lua
@@ -1,37 +1,68 @@
 local plugin_name = "cmp"
 local utils_plug = require("utils.plugin")
-
-local snippet_library = "vsnip"
--- local snippet_library = "luasnip"
 if not utils_plug.is_exists(plugin_name) then
   return
 end
 
+local snippet_library = "vsnip"
+-- local snippet_library = "luasnip"
+
 vim.o.completeopt = "menuone,noinsert,noselect"
 
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 local function loading()
-  local cmp = require(plugin_name)
-  local types = require("cmp.types")
   local utils = require("utils")
   local force_require = require("utils.plugin").force_require
+  local cmp = require(plugin_name)
+  local types = require("cmp.types")
   local t = utils.t
-  local toboolean = utils.toboolean
+  local tb = utils.toboolean
   local vsnip_jumpable = vim.fn["vsnip#jumpable"]
   local _, luasnip = pcall(require, "luasnip")
-  local snl = snippet_library
+
+  local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  end
+
+  local is_emmet_active = function()
+    local clients = vim.lsp.buf_get_clients()
+
+    for _, client in pairs(clients) do
+      if client.name == "emmet_ls" then
+        return true
+      end
+    end
+    return false
+  end
+
+  local snippet_jumpable = function(dir)
+    if snippet_library == "vsnip" then
+      return tb(vsnip_jumpable(dir))
+    elseif snippet_library == "luasnip" then
+      return tb(luasnip.jumpable(dir))
+    end
+    return false
+  end
+
+  local snippet_jump = function(dir)
+    if snippet_library == "vsnip" then
+      if dir == 1 then
+        vim.fn.feedkeys(t("<Plug>(vsnip-jump-next)"), "")
+      elseif dir == -1 then
+        vim.fn.feedkeys(t("<Plug>(vsnip-jump-prev)"), "")
+      end
+    elseif snippet_library == "luasnip" then
+      luasnip.jump(dir)
+    end
+  end
 
   local setup_opt = {
     snippet = {
       -- REQUIRED - you must specify a snippet engine
       expand = function(args)
-        if snl == "vsnip" then
+        if snippet_library == "vsnip" then
           vim.fn["vsnip#anonymous"](args.body)
-        elseif snl == "luasnip" then
+        elseif snippet_library == "luasnip" then
           luasnip.lsp_expand(args.body)
         end
       end,
@@ -42,13 +73,19 @@ local function loading()
         i = cmp.mapping.abort(),
         c = cmp.mapping.close(),
       }),
+      ["<C-y>"] = cmp.config.disable,
       ["<C-j>"] = cmp.mapping.select_next_item(),
       ["<C-k>"] = cmp.mapping.select_prev_item(),
       ["<C-d>"] = cmp.mapping.scroll_docs(-4),
       ["<C-f>"] = cmp.mapping.scroll_docs(4),
       ["<CR>"] = function(fallback)
-        if cmp.visible() and cmp.get_selected_entry() ~= nil then
-          cmp.confirm({ select = false })
+        local entry = cmp.get_selected_entry()
+        if cmp.visible() and entry ~= nil then
+          local confirm_option = {
+            select = false,
+            behavior = entry.source.name == "copilot" and cmp.ConfirmBehavior.Insert or cmp.ConfirmBehavior.Replace,
+          }
+          cmp.confirm(confirm_option)
         else
           fallback() -- If you are using vim-endwise, this fallback function will be behaive as the vim-endwise.
         end
@@ -56,12 +93,14 @@ local function loading()
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_next_item()
-        elseif snl == "vsnip" and toboolean(vsnip_jumpable(1)) then
-          vim.fn.feedkeys(t("<Plug>(vsnip-jump-next)"), "")
-        elseif snl == "luasnip" and luasnip.jumpable(1) then
-          luasnip.jump(1)
+        elseif snippet_jumpable(1) then
+          snippet_jump(1)
         elseif has_words_before() then
           cmp.complete()
+          -- elseif vim.g.loaded_copilot then
+          --   vim.api.nvim_feedkeys(vim.fn["copilot#Accept"](t("<Tab>")), "n", true)
+          -- elseif is_emmet_active() then
+          --   cmp.complete()
         else
           fallback()
         end
@@ -70,10 +109,8 @@ local function loading()
       ["<S-Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
           cmp.select_prev_item()
-        elseif snl == "vsnip" and toboolean(vsnip_jumpable(-1)) then
-          vim.fn.feedkeys(t("<Plug>(vsnip-jump-prev)"), "")
-        elseif snl == "luasnip" and luasnip.jumpable(-1) then
-          luasnip.jump(-1)
+        elseif snippet_jumpable(-1) then
+          snippet_jump(-1)
         elseif vim.g.loaded_copilot then
           vim.api.nvim_feedkeys(vim.fn["copilot#Accept"](t("<Tab>")), "n", true)
         else
@@ -116,22 +153,23 @@ local function loading()
 
     sources = cmp.config.sources({
       { name = "copilot" },
-      { name = "nvim_lsp" },
-      { name = "path" },
-      { name = "rg" },
+      -- { name = "rg" },
       { name = "vsnip" },
       { name = "luasnip" },
-      { name = "nvim_lsp_document_symbol" },
-      { name = "treesitter" },
+      { name = "nvim_lsp" },
+      { name = "path" },
+      { name = "emoji", insert = true },
       { name = "nvim_lua" },
-      { name = "calc" },
-      { name = "spell" },
-      { name = "emoji" },
-      { name = "look", keyword_length = 2, option = { convert_case = true, loud = true } },
       { name = "nvim_lsp_signature_help" },
     }, {
       { name = "buffer" },
       { name = "omni" },
+      { name = "calc" },
+      { name = "spell" },
+      { name = "treesitter" },
+      { name = "git" },
+      { name = "dictionary", keyword_length = 2 },
+      { name = "look", keyword_length = 2, option = { convert_case = true, loud = true } },
       -- { name = "cmp_tabnine" },
     }),
     completion = {
@@ -156,8 +194,7 @@ local function loading()
           ultisnips = "[UltiSnips]",
           vsnip = "[vSnip]",
           luasnip = "[LuaSnip]",
-          treesitter = "[treesitter]",
-          look = "[Look]",
+          treesitter = "[TS]",
           spell = "[Spell]",
           calc = "[Calc]",
           emoji = "[Emoji]",
@@ -168,6 +205,10 @@ local function loading()
           nvim_lsp_signature_help = "[Signature]",
           copilot = "[Copilot]",
           cmdline_history = "[History]",
+          mocword = "[Mocword]",
+          dictionary = "[Dictionary]",
+          look = "[Look]",
+          git = "[Git]",
           -- cmp_openai_codex = "[Codex]",
         },
       }),
@@ -175,6 +216,28 @@ local function loading()
   end
 
   cmp.setup(setup_opt)
+
+  cmp.setup.filetype({ "gitcommit", "markdown" }, {
+    sources = cmp.config.sources({
+      { name = "copilot" },
+      -- { name = "rg" },
+      { name = "vsnip" },
+      { name = "luasnip" },
+      { name = "nvim_lsp" },
+      { name = "path" },
+      { name = "emoji", insert = true },
+    }, {
+      { name = "buffer" },
+      { name = "omni" },
+      { name = "spell" },
+      { name = "calc" },
+      { name = "treesitter" },
+      { name = "git" },
+      { name = "mocword" },
+      { name = "dictionary", keyword_length = 2 },
+      { name = "look", keyword_length = 2, option = { convert_case = true, loud = true } },
+    }),
+  })
 
   cmp.setup.cmdline("/", {
     mapping = cmp.mapping.preset.cmdline(),
@@ -210,6 +273,12 @@ local function loading()
       run_on_every_keystroke = true,
       snippet_placeholder = "..",
     })
+  end
+
+  -- Setup git
+  local status_git, _ = force_require("cmp-git")
+  if status_git then
+    require("cmp_git").setup()
   end
 
   -- Setup autopairs
