f4d9a6c3383a3e1e20d9d291995590315b2d6649
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-06-12 17:38:50 +0100

    print-debug-fix

---
+++ .config/nvim/lua/plugin/config/vim-print-debug.lua
@@ -0,0 +1,21 @@
+local plugin_name = "vim-print-debug"
+if not require("utils.plugin").is_exists(plugin_name) then
+  return
+end
+
+local function loading()
+  vim.keymap.set("n", "sp", "<cmd>call print_debug#print_debug()<cr>", { noremap = true })
+  -- vim.g.print_debug_templates = {
+  --   go = 'fmt.Printf("+++ {}\n")',
+  --   python = 'print(f"+++ {}")',
+  --   javascript = "console.log(`+++ {}`);",
+  --   typescript = "console.log(`+++ {}`);",
+  --   typescriptreact = "console.log(`+++ {}`);",
+  --   c = 'printf("+++ {}\n");',
+  --   rust = 'println!("+++ {}\n");',
+  --   sh = 'echo "+++ {}"',
+  --   zsh = 'echo "+++ {}"',
+  -- }
+end
+
+require("utils.plugin").force_load_on_event(plugin_name, loading)
