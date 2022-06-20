e22c3d37656858753cbb181d239ef12d54a82f9c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-06-20 15:13:29 +0100

    lspkind fix

--- .config/nvim/lua/plugin/config/nvim-navic.lua
+++ .config/nvim/lua/plugin/config/nvim-navic.lua
@@ -1,8 +1,12 @@
 local plugin_name = "nvim-navic"
 
 local function loading()
+  local symbol_map = {}
+  for key, value in pairs(require("lspkind").symbol_map) do
+    symbol_map[value] = key .. " "
+  end
   require(plugin_name).setup({
-    icons = require("lspkind").symbol_map,
+    icons = symbol_map,
     highlight = false,
     separator = " > ",
     depth_limit = 0,
