ec20c3332309ad53bfb884883cd9370daf52be87
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-06-23 17:52:41 +0100

    fix lazy loading

--- .config/nvim/lua/plugin/config/lspkind.lua
+++ .config/nvim/lua/plugin/config/lspkind.lua
@@ -1,52 +1,44 @@
-local plugin_name = "lspkind"
+require("lspkind").init({
+  -- enables text annotations
+  --
+  -- default: true
+  -- with_text = true
 
-local function loading()
-  require(plugin_name).init({
-    -- enables text annotations
-    --
-    -- default: true
-    -- with_text = true
+  -- default symbol map
+  -- can be either 'default' (requires nerd-fonts font) or
+  -- 'codicons' for codicon preset (requires vscode-codicons font)
+  --
+  -- default: 'default'
+  preset = "codicons",
 
-    -- default symbol map
-    -- can be either 'default' (requires nerd-fonts font) or
-    -- 'codicons' for codicon preset (requires vscode-codicons font)
-    --
-    -- default: 'default'
-    preset = "codicons",
-
-    -- override preset symbols
-    --
-    -- default: {}
-    symbol_map = {
-      Text = " ",
-      Method = " ",
-      Function = " ",
-      Constructor = " ",
-      Field = "ﰠ ",
-      Variable = " ",
-      Class = "ﴯ ",
-      Interface = " ",
-      Module = " ",
-      Property = "ﰠ ",
-      Unit = " ",
-      Value = " ",
-      Enum = " ",
-      Keyword = " ",
-      Snippet = " ",
-      Color = " ",
-      File = " ",
-      Reference = " ",
-      Folder = " ",
-      EnumMember = " ",
-      Constant = " ",
-      Struct = "פּ ",
-      Event = "",
-      Operator = " ",
-      TypeParameter = " ",
-    },
-  })
-end
-
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = loading,
+  -- override preset symbols
+  --
+  -- default: {}
+  symbol_map = {
+    Text = " ",
+    Method = " ",
+    Function = " ",
+    Constructor = " ",
+    Field = "ﰠ ",
+    Variable = " ",
+    Class = "ﴯ ",
+    Interface = " ",
+    Module = " ",
+    Property = "ﰠ ",
+    Unit = " ",
+    Value = " ",
+    Enum = " ",
+    Keyword = " ",
+    Snippet = " ",
+    Color = " ",
+    File = " ",
+    Reference = " ",
+    Folder = " ",
+    EnumMember = " ",
+    Constant = " ",
+    Struct = "פּ ",
+    Event = "",
+    Operator = " ",
+    TypeParameter = " ",
+  },
 })
