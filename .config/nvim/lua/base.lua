aa32a21590b455f1ce54ee4b0dd0282086fb0317
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-06-23 17:31:39 +0100

    add base

---
+++ .config/nvim/lua/base.lua
@@ -0,0 +1,33 @@
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_matchparen = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_remote_plugins = 1
+vim.g.loaded_shada_plugin = 1
+vim.g.loaded_spellfile_plugin = 1
+vim.g.loaded_spellfile_plugin = 1
+vim.g.loaded_spellfile_plugin = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_tutor_mode_plugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+
+vim.g.load_black = 1
+vim.g.loaded_fzf = 1
+vim.g.loaded_gtags = 1
+vim.g.loaded_gtags_cscope = 1
+
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_python_provider = 0
+vim.g.loaded_pythonx_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.do_filetype_lua = 1
+vim.g.did_load_filetypes = 0
