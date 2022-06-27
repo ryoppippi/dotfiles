497c8e714ac3e5439b05eafe8776f8545ff59a43
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-06-27 22:02:41 +0100

    fix colorizer

--- .config/nvim/lua/plugin/config/nvim-colorizer.lua
+++ .config/nvim/lua/plugin/config/nvim-colorizer.lua
@@ -1,8 +1,5 @@
-local plugin_name = "colorizer"
-
 local function loading()
-  require(plugin_name).setup()
-  pcall(vim.cmd, [[ColorizerToggle]])
+  require("colorizer").setup()
 end
 
-require("utils.plugin").force_load_on_event(plugin_name, loading)
+require("utils.plugin").force_load_on_event("nvim-colorizer", loading)
