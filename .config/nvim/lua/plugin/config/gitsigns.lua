abaff225d8b6db097d2f5428a8b43348a7475af2
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-06-28 10:19:09 +0100

    git keyconfig setup

--- .config/nvim/lua/plugin/config/gitsigns.lua
+++ .config/nvim/lua/plugin/config/gitsigns.lua
@@ -11,8 +11,16 @@ local function loading()
     },
     current_line_blame = true,
     on_attach = function(buffer)
-      vim.keymap.set("n", "<leader>gh", "<cmd>Gitsigns preview_hunk<cr>", { silent = true })
-      vim.keymap.set("n", "<leader>gd", "<cmd>Gitsigns diffthis<cr>", { silent = true })
+      local opts = { silent = true, buffer = buffer }
+      vim.keymap.set("n", "<leader>gS", "<cmd>Gitsigns stage_buffer<cr>", opts)
+      vim.keymap.set("n", "<leader>gh", "<cmd>Gitsigns preview_hunk<cr>", opts)
+      vim.keymap.set("n", "<leader>gs", "<cmd>Gitsigns stage_hunk<cr>", opts)
+      vim.keymap.set("n", "<leader>gu", "<cmd>Gitsigns undo_stage_hunk<cr>", opts)
+      vim.keymap.set("n", "<leader>gr", "<cmd>Gitsigns reset_hunk<cr>", opts)
+      vim.keymap.set("n", "<leader>gR", "<cmd>Gitsigns reset_buffer<cr>", opts)
+      vim.keymap.set({ "o", "x" }, "ih", "<cmd><c-u>Gitsigns select_hunk<cr>", opts)
+      vim.keymap.set("n", "<leader>gd", "<cmd>Gitsigns diffthis<cr>", opts)
+      -- vim.keymap.set("n", "<leader>gm", "<cmd>Gitsigns blame_line<cr>", opts)
     end,
   })
 end
