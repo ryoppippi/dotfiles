95d2ecbbd8b093783ad6ecba322f29e119d8450e
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-07-02 14:21:47 +0100

    fix copy/paste

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -110,8 +110,8 @@ abbr -a dcd "docker-compose down"
 abbr -a dcr "docker-compose restart"
 abbr -a gg googler
 
-abbr -a cpf "pbcopy < cat"
-abbr -a paf "pbpaste >"
+abbr -a cpf "pbcopy < "
+abbr -a paf "pbpaste > "
 # abbr -a pyenv "env CC=/usr/bin/gcc CXX=/usr/bin/g++  PYTHON_CONFIGURE_OPTS='--enable-framework --enable-toolbox-glue --enable-big-digits --enable-unicode --with-threads' pyenv"
 
 # if test (uname -m) = arm64
