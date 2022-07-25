8b4073d2977f66963adac64ff3b1d5d9936dc53c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-07-25 10:03:09 +0100

    make toboolean global

--- .config/nvim/lua/global.lua
+++ .config/nvim/lua/global.lua
@@ -1,1 +1,13 @@
-_G.tb = require("utils").toboolean
+_G.tb = function(value)
+  if value == nil then
+    return false
+  elseif type(value) == "boolean" then
+    return value
+  elseif type(value) == "number" then
+    return value ~= 0
+  elseif type(value) == "string" then
+    return string.lower(value) == "true"
+  else
+    return false
+  end
+end
