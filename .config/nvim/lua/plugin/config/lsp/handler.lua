7ebfc5d220af40378d37ea808551e5b1495f8905
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-08-08 11:02:15 +0100

    fix diagnostic keymap

--- .config/nvim/lua/plugin/config/lsp/handler.lua
+++ .config/nvim/lua/plugin/config/lsp/handler.lua
@@ -26,11 +26,10 @@ local function set_keymap(client, bufnr)
 
   -- diagnostics
   vim.keymap.set("n", "gj", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
-  -- vim.keymap.set("n", "gJ", [[<cmd>Telescope diagnostics<cr>]], opts)
-  vim.keymap.set("n", "gJ", [[<cmd> lua vim.lsp.diagnostic.set_loclist()<cr>]], opts)
+  vim.keymap.set("n", "gJ", [[<cmd>Telescope diagnostics<cr>]], opts)
+  vim.keymap.set("n", "gl", [[<cmd>lua vim.diagnostic.setloclist()<cr>]], opts)
   vim.keymap.set("n", "-", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
   vim.keymap.set("n", "_", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
-  -- vim.keymap.set("n", "gl", "<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>", opts)
 
   -- rename
   -- vim.keymap.set("n", "cW", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
