d3f7e2db0ced6645486b8af212809cc3b70011c2
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-08-12 12:56:18 +0100

    add open Term commands

--- .config/nvim/lua/command.lua
+++ .config/nvim/lua/command.lua
@@ -63,13 +63,34 @@ vim.api.nvim_create_user_command("H", function(tbl)
 end, { nargs = "?", complete = "help", force = true })
 
 -- terminal
-vim.api.nvim_create_user_command("T", function(tbl)
-  local args = tbl.args
+local function openTerm(args)
   local tf = vim.cmd.terminal
-  vim.cmd.tabe()
   if args == "" then
     tf()
   else
     tf(args)
   end
+end
+vim.api.nvim_create_user_command("T", function(tbl)
+  local args = tbl.args
+  vim.cmd.tabe()
+  openTerm(args)
+end, { nargs = "*" })
+
+vim.api.nvim_create_user_command("TS", function(tbl)
+  local args = tbl.args
+  local splitbelow = vim.o.splitbelow
+  vim.opt.splitbelow = true
+  vim.cmd.split()
+  openTerm(args)
+  vim.opt.splitbelow = splitbelow
+end, { nargs = "*" })
+
+vim.api.nvim_create_user_command("TV", function(tbl)
+  local args = tbl.args
+  local splitright = vim.o.splitright
+  vim.opt.splitright = true
+  vim.cmd.vsplit()
+  openTerm(args)
+  vim.opt.splitright = splitright
 end, { nargs = "*" })
