5dd1a7df569963a0fad1cb14482670a69cd01bc5
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-09-20 10:44:41 +0100

    fix copilot

--- .config/nvim/lua/plugin/config/copilot.lua
+++ .config/nvim/lua/plugin/config/copilot.lua
@@ -1,6 +1,3 @@
--- vim.api.nvim_command('imap <expr> <Plug>(copilot-dummy-map) copilot#Accept("<Tab>")')
--- vim.api.nvim_command("imap <silent><script><nowait><expr> <C-l> copilot#Dismiss()")
--- vim.api.nvim_command('imap <expr> <C-h> copilot#Accept("<CR>")')
 vim.keymap.set("i", "<Plug>(copilot-dummy-map)", [[copilot#Accept"]("<Tab>")]], { expr = true })
 vim.keymap.set("i", "<C-h>", [[copilot#Accept("<CR>")]], { expr = true })
 vim.keymap.set("i", "<C-l>", [[copilot#Dismiss()]], { script = true, nowait = true, expr = true })
@@ -9,3 +6,5 @@ vim.g.copilot_no_tab_map = true
 vim.g.copilot_assume_mapped = true
 vim.g.copilot_tab_fallback = ""
 vim.g.copilot_filetypes = { ["*"] = true }
+-- vim.g.copilot_ignore_node_version = true
+-- vim.g.copilot_node_command = "bun"
