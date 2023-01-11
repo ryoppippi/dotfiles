9950717e7cb578ec2990e16f46e9a06515c79261
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-01-11 13:03:00 +0900

    split plugins

--- .config/nvim/lua/plugin/config/mini.lua
+++ .config/nvim/lua/plugin/config/mini.lua
@@ -1,28 +1,42 @@
 return {
-  "echasnovski/mini.nvim",
-  branch = "stable",
-  -- lazy = false,
-  event = "VeryLazy",
-  config = function()
-    require("mini.indentscope").setup({})
-
-    require("mini.jump").setup({
-      mappings = {
-        forward = "f",
-        backward = "F",
-        forward_till = "",
-        backward_till = "",
-        repeat_jump = "",
-      },
-      delay = {
-        highlight = 10,
-      },
-    })
-
-    require("mini.trailspace").setup({})
-    vim.api.nvim_create_user_command("TrimSpace", MiniTrailspace.trim, {})
-    -- setup('mini.cursorword',{})
-
-    -- setup("mini.jump2d", { mappings = { start_jumping = "<cr>" } })
-  end,
+  {
+    "echasnovski/mini.trailspace",
+    branch = "stable",
+    keys = { "TrimSpace" },
+    init = function()
+      vim.api.nvim_create_user_command("TrimSpace", function()
+        require("mini.trailspace").trim()
+      end, {})
+    end,
+    config = function()
+      require("mini.trailspace").setup({})
+    end,
+  },
+  {
+    "echasnovski/mini.indentscope",
+    branch = "stable",
+    event = "VeryLazy",
+    config = function()
+      require("mini.indentscope").setup({})
+    end,
+  },
+  {
+    "echasnovski/mini.jump",
+    branch = "stable",
+    event = "BufEnter",
+    config = function()
+      require("mini.jump").setup({
+        mappings = {
+          forward = "f",
+          backward = "F",
+          forward_till = "",
+          backward_till = "",
+          repeat_jump = "",
+        },
+        delay = {
+          highlight = 10,
+        },
+      })
+    end,
+  },
 }
