b029328ea68fb6c3983da2db7d8e07a4d35c338b
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-01-11 23:20:24 +0900

    set concurrency

--- dot_config/nvim/lua/plugin/lazy.lua
+++ dot_config/nvim/lua/plugin/lazy.lua
@@ -1,38 +1,39 @@
 -- check if lazy.nvim is installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--single-branch",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--single-branch",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
 end
 vim.opt.runtimepath:prepend(lazypath)
 
 local lazy = require("lazy")
 -- load plugins
 lazy.setup("plugin.config", {
-  defaults = { lazy = true },
-  install = { missing = true, colorscheme = { "kanagawa" } },
-  checker = { enabled = false },
-  performance = {
-    cache = {
-      enabled = true,
-    },
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        "matchit",
-        "matchparen",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
+	defaults = { lazy = true },
+	install = { missing = true, colorscheme = { "kanagawa" } },
+	checker = { enabled = false },
+	concurrency = 16,
+	performance = {
+		cache = {
+			enabled = true,
+		},
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				"matchparen",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
 })
