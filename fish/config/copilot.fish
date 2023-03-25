6ee4b459935df906ead7a441360926d6fb1c1954
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-03-25 13:41:19 +0000

    update fish

---
+++ fish/config/copilot.fish
@@ -0,0 +1,25 @@
+function github-copilot_helper
+    set -l TMPFILE (mktemp)
+    trap 'rm -f $TMPFILE' EXIT
+    if github-copilot-cli $argv[1] "$argv[2..]" --shellout $TMPFILE
+        if [ -e "$TMPFILE" ]
+            set -l FIXED_CMD (cat $TMPFILE)
+            eval "$FIXED_CMD"
+        else
+            echo "Apologies! Extracting command failed"
+        end
+    else
+        return 1
+    end
+end
+
+function qf
+    set -l prev_cmd (history | head -n 1)
+    echo "Previous command: $prev_cmd"
+    github-copilot_helper what-the-shell "correct the following commands {$prev_cmd}"
+end
+
+
+alias q 'github-copilot_helper what-the-shell'
+alias qgit 'github-copilot_helper git-assist'
+alias qgh 'github-copilot_helper gh-assist'
