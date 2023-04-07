54ace38987accbd1168eb44757b49cbc1827f405
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-04-07 22:30:20 +0100

    update winbar

--- nvim/lua/plugin/config/heirline/components/FileName.lua
+++ nvim/lua/plugin/config/heirline/components/FileName.lua
@@ -4,6 +4,15 @@ return {
   init = function(self)
     self.filename = vim.api.nvim_buf_get_name(0)
   end,
+  hl = function()
+    local opt = { bold = true }
+    if vim.bo.modified then
+      -- use `force` because we need to override the child's hl foreground
+      opt.fg = "cyan"
+      opt.force = true
+    end
+    return opt
+  end,
   provider = function(self)
     local filename = vim.fn.fnamemodify(self.filename, ":.")
     if filename == "" then
@@ -14,5 +23,4 @@ return {
     end
     return filename
   end,
-  hl = { bold = true },
 }
