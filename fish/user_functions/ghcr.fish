45133e6f7d57a57b83064886670cd9a44ca6e819
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-04-12 19:18:04 +0100

    fix fish functions

--- fish/user_functions/ghcr.fish
+++ fish/user_functions/ghcr.fish
@@ -2,5 +2,5 @@ function ghcr
     gh repo create $argv
     ghq get $argv[1]
     cd (ghq list --full-path -e $argv[1])
-    $EDITOR (ghq list --full-path -e $argv[1])
+    # $EDITOR (ghq list --full-path -e $argv[1])
 end
