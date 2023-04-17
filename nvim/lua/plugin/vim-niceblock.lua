f189e7162340d240da3a01edab5a1cc1e4e2b4a1
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-04-17 14:47:13 +0100

    fix keymaps

--- nvim/lua/plugin/vim-niceblock.lua
+++ nvim/lua/plugin/vim-niceblock.lua
@@ -1,7 +1,7 @@
 return {
   "kana/vim-niceblock",
   keys = {
-    { "I", "<Plug>(niceblock-I)", { "x" } },
-    { "A", "<Plug>(niceblock-A)", { "x" } },
+    { "I", "<Plug>(niceblock-I)", mode = { "x" } },
+    { "A", "<Plug>(niceblock-A)", mode = { "x" } },
   },
 }
