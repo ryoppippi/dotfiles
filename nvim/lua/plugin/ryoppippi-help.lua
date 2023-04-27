a45532b72ad903afa7d8360e8e4c19ebbba70ecb
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-04-27 19:39:44 +0100

    move my commands to plugin dir

---
+++ nvim/lua/plugin/ryoppippi-help.lua
@@ -0,0 +1,48 @@
+return {
+  dir = "",
+  name = "ryoppippi-help.nvim",
+  dependencies = { "MunifTanjim/nui.nvim" },
+  cmd = { "H" },
+  config = function()
+    vim.api.nvim_create_user_command("H", function(tbl)
+      local args = tbl.args
+      local Popup = require("nui.popup")
+      if not Popup then
+        vim.cmd.tabnew()
+        vim.bo.buftype = "help"
+        vim.cmd.help(args)
+        return
+      end
+      local popup = Popup({
+        enter = true,
+        focusable = true,
+        border = {
+          style = "rounded",
+        },
+        relative = "editor",
+        position = "50%",
+        size = {
+          width = "80%",
+          height = "80%",
+        },
+        buf_options = {
+          modifiable = false,
+          readonly = true,
+          buftype = "help",
+        },
+      })
+      local unmount = function()
+        popup:unmount()
+      end
+
+      popup:mount()
+      vim.cmd.help(args)
+      popup:on("BufLeave", function()
+        popup:unmount()
+      end)
+      for _,v in ipairs({ "<esc>", "q" }) do
+        popup:map("n", v, unmount, { noremap = true })
+      end
+    end, { nargs = "?", complete = "help", force = true })
+  end,
+}
