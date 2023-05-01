7092ab3ab7721ef5176fd3bb45e6acfda78e7418
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-05-01 17:45:43 +0100

    fix code action preview keymap

--- nvim/lua/plugin/nvim-lspconfig/keymaps.lua
+++ nvim/lua/plugin/nvim-lspconfig/keymaps.lua
@@ -1,7 +1,9 @@
+local has = require("core.plugin").has
 return {
-  on_attach = function(client, bufnr)
+  on_attach = function(_, bufnr)
     local opts = { silent = true, buffer = bufnr }
-    local telescope_available = require("core.plugin").has("telescope.nvim")
+    local has_telescope = has("telescope.nvim")
+    local has_action_preview = has("actions-preview.nvim")
 
     -- hover doc
     vim.keymap.set("n", "gh", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
@@ -9,7 +11,7 @@ return {
     -- jump to
     vim.keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<cr>", opts)
 
-    if telescope_available then
+    if has_telescope then
       vim.keymap.set("n", "gd", [[<cmd>Telescope lsp_definitions<cr>]], opts)
       vim.keymap.set("n", "gt", [[<cmd>Telescope lsp_type_definitions<cr>]], opts)
       vim.keymap.set("n", "gI", [[<cmd>Telescope lsp_implementations<cr>]], opts)
@@ -26,9 +28,10 @@ return {
 
     -- diagnostics
     vim.keymap.set("n", "gL", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
-    if telescope_available then
+    if has_telescope then
       vim.keymap.set("n", "gl", [[<cmd>Telescope diagnostics<cr>]], opts)
     end
+
     vim.keymap.set("n", "-", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
     vim.keymap.set("n", "_", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
 
@@ -43,7 +46,9 @@ return {
     end, { expr = true, buffer = bufnr, desc = "rename words" })
 
     -- code actions
-    vim.keymap.set({ "n", "v", "x" }, "ga", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+    if not has_action_preview then
+      vim.keymap.set({ "n", "v", "x" }, "ga", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+    end
 
     -- workspace
     vim.keymap.set("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>", opts)
