60a5290cf06892416e1d036129b015ecb455f942
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-05-02 09:09:53 +0100

    oil keyconfig

--- nvim/lua/plugin/oil.lua
+++ nvim/lua/plugin/oil.lua
@@ -4,6 +4,33 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
-  opts = {},
-  config = true,
+  keys = {
+    {
+      "<leader>e",
+      function()
+        require("oil").open_float(".")
+      end,
+    },
+  },
+  opts = {
+    keymaps = {
+      ["g?"] = "actions.show_help",
+      ["<CR>"] = "actions.select",
+      ["<C-p>"] = "actions.preview",
+      ["q"] = "actions.close",
+      ["<C-l>"] = "actions.refresh",
+      ["-"] = "actions.parent",
+      ["_"] = "actions.open_cwd",
+      ["`"] = "actions.cd",
+      ["~"] = "actions.tcd",
+      ["g."] = "actions.toggle_hidden",
+      -- ["<C-s>"] = "actions.select_vsplit",
+      -- ["<C-h>"] = "actions.select_split",
+      -- ["<C-t>"] = "actions.select_tab",
+    },
+    view_options = {
+      show_hidden = true,
+    },
+    use_default_keymaps = false,
+  },
 }
