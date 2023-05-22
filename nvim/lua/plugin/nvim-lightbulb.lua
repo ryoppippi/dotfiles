362c8605d804f5543378fb218c7479d30aa4c156
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-05-22 18:43:14 +0100

    disable lightbulb

--- nvim/lua/plugin/nvim-lightbulb.lua
+++ nvim/lua/plugin/nvim-lightbulb.lua
@@ -1,6 +1,7 @@
 return {
   "kosayoda/nvim-lightbulb",
   event = { "LspAttach" },
+  enabled = false,
   config = function()
     require("nvim-lightbulb").setup({ autocmd = { enabled = true } })
   end,
