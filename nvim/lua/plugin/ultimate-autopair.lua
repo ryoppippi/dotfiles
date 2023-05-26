a1c8386a094124cb2ce7155eeeffa0393a7c74f5
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-05-26 19:32:25 +0100

    add ultimate autopair

---
+++ nvim/lua/plugin/ultimate-autopair.lua
@@ -0,0 +1,10 @@
+return {
+  "altermo/ultimate-autopair.nvim",
+  lazy = false,
+  dependencies = {
+    { "altermo/npairs-integrate-upair", opts = { map = "u" } },
+    "windwp/nvim-autopairs",
+  },
+  event = { "InsertEnter", "CmdlineEnter" },
+  -- opts = {},
+}
