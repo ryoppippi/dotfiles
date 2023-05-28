8ae56fd55d3c2e74a87d0a24eeb560bb0ac855fb
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-05-28 18:11:14 +0100

    fix stylua format

--- nvim/after/ftplugin/toml.lua
+++ nvim/after/ftplugin/toml.lua
@@ -2,8 +2,8 @@ require("lazy").load({ plugins = { "nvim-cmp" } })
 local cmp = require("cmp")
 local sources = cmp.get_config().sources
 for i = #sources, 1, -1 do
-  if sources[i].name == "treesitter" then
-    table.remove(sources, i)
-  end
+	if sources[i].name == "treesitter" then
+		table.remove(sources, i)
+	end
 end
 cmp.setup.buffer({ sources = sources })
--- wezterm/colors/kanagawa.lua
+++ wezterm/colors/kanagawa.lua
@@ -1,65 +1,65 @@
 return {
-  force_reverse_video_cursor = true,
-  use_fancy_tab_bar = false,
-  colors = {
-    foreground = "#dcd7ba",
-    background = "#1f1f28",
+	force_reverse_video_cursor = true,
+	use_fancy_tab_bar = false,
+	colors = {
+		foreground = "#dcd7ba",
+		background = "#1f1f28",
 
-    cursor_bg = "#c8c093",
-    cursor_fg = "#c8c093",
-    cursor_border = "#c8c093",
+		cursor_bg = "#c8c093",
+		cursor_fg = "#c8c093",
+		cursor_border = "#c8c093",
 
-    selection_fg = "#c8c093",
-    selection_bg = "#2d4f67",
+		selection_fg = "#c8c093",
+		selection_bg = "#2d4f67",
 
-    scrollbar_thumb = "#16161d",
-    split = "#16161d",
+		scrollbar_thumb = "#16161d",
+		split = "#16161d",
 
-    ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
-    brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
-    indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
+		ansi = { "#090618", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093" },
+		brights = { "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba" },
+		indexed = { [16] = "#ffa066", [17] = "#ff5d62" },
 
-    tab_bar = {
-      background = "#1b1f2f",
+		tab_bar = {
+			background = "#1b1f2f",
 
-      active_tab = {
-        bg_color = "#444b71",
-        fg_color = "#c6c8d1",
-        intensity = "Normal",
-        underline = "None",
-        italic = false,
-        strikethrough = false,
-      },
+			active_tab = {
+				bg_color = "#444b71",
+				fg_color = "#c6c8d1",
+				intensity = "Normal",
+				underline = "None",
+				italic = false,
+				strikethrough = false,
+			},
 
-      inactive_tab = {
-        bg_color = "#282d3e",
-        fg_color = "#c6c8d1",
-        intensity = "Normal",
-        underline = "None",
-        italic = false,
-        strikethrough = false,
-      },
+			inactive_tab = {
+				bg_color = "#282d3e",
+				fg_color = "#c6c8d1",
+				intensity = "Normal",
+				underline = "None",
+				italic = false,
+				strikethrough = false,
+			},
 
-      inactive_tab_hover = {
-        bg_color = "#1b1f2f",
-        fg_color = "#c6c8d1",
-        intensity = "Normal",
-        underline = "None",
-        italic = true,
-        strikethrough = false,
-      },
+			inactive_tab_hover = {
+				bg_color = "#1b1f2f",
+				fg_color = "#c6c8d1",
+				intensity = "Normal",
+				underline = "None",
+				italic = true,
+				strikethrough = false,
+			},
 
-      new_tab = {
-        bg_color = "#1b1f2f",
-        fg_color = "#c6c8d1",
-        italic = false,
-      },
+			new_tab = {
+				bg_color = "#1b1f2f",
+				fg_color = "#c6c8d1",
+				italic = false,
+			},
 
-      new_tab_hover = {
-        bg_color = "#444b71",
-        fg_color = "#c6c8d1",
-        italic = false,
-      },
-    },
-  },
+			new_tab_hover = {
+				bg_color = "#444b71",
+				fg_color = "#c6c8d1",
+				italic = false,
+			},
+		},
+	},
 }
