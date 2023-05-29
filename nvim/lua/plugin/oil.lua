de5d86e58abd320f32a5aca089534ae31ddb7212
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-05-29 10:27:01 +0100

    use trash command

--- nvim/lua/plugin/oil.lua
+++ nvim/lua/plugin/oil.lua
@@ -20,27 +20,33 @@ return {
 			require("oil").open(vim.fn.expand("%:p:h"))
 		end
 	end,
-	opts = {
-		keymaps = {
-			["g?"] = "actions.show_help",
-			["<CR>"] = "actions.select",
-			["-"] = "actions.parent",
-			["<C-p>"] = "actions.preview",
-			["q"] = "actions.close",
-			["<C-l>"] = "actions.refresh",
-			["<space>"] = "actions.open_cwd",
-			["`"] = "actions.cd",
-			["~"] = "actions.tcd",
-			["g."] = "actions.toggle_hidden",
-			-- ["<C-s>"] = "actions.select_vsplit",
-			-- ["<C-h>"] = "actions.select_split",
-			-- ["<C-t>"] = "actions.select_tab",
-		},
-		view_options = {
-			show_hidden = true,
-		},
-		use_default_keymaps = false,
-	},
+	opts = function()
+		local trash_command = "trash"
+		local is_trash = tb(vim.fn.executable(trash_command))
+		return {
+			keymaps = {
+				["g?"] = "actions.show_help",
+				["<CR>"] = "actions.select",
+				["-"] = "actions.parent",
+				["<C-p>"] = "actions.preview",
+				["q"] = "actions.close",
+				["<C-l>"] = "actions.refresh",
+				["<space>"] = "actions.open_cwd",
+				["`"] = "actions.cd",
+				["~"] = "actions.tcd",
+				["g."] = "actions.toggle_hidden",
+				-- ["<C-s>"] = "actions.select_vsplit",
+				-- ["<C-h>"] = "actions.select_split",
+				-- ["<C-t>"] = "actions.select_tab",
+			},
+			view_options = {
+				show_hidden = true,
+			},
+			use_default_keymaps = false,
+			delete_to_trash = is_trash,
+			trash_command = trash_command,
+		}
+	end,
 	config = function(_, opts)
 		require("oil").setup(opts)
 	end,
