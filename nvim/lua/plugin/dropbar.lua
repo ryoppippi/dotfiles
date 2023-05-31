2010d006bda9b59da49592d967447698408ff468
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-05-31 17:11:30 +0100

    add keyconfig

--- nvim/lua/plugin/dropbar.lua
+++ nvim/lua/plugin/dropbar.lua
@@ -2,9 +2,16 @@ return {
 	"Bekaboo/dropbar.nvim",
 	event = "VeryLazy",
 	cond = tb(vim.fn.has("nvim-0.10")),
-	enabled = false,
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
+	},
+	keys = {
+		{
+			"<Leader>m",
+			function()
+				require("dropbar.api").pick()
+			end,
+		},
 	},
 	config = true,
 }
