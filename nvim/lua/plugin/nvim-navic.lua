199b7531a0b2b574ea4cd88d00ed82dc920a6d08
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-06-02 15:02:35 +0100

    fix lazy load of navic

--- nvim/lua/plugin/nvim-navic.lua
+++ nvim/lua/plugin/nvim-navic.lua
@@ -5,15 +5,15 @@ return {
 		"onsails/lspkind.nvim",
 	},
 	init = function()
-		vim.g.navic_silence = true
-
 		require("core.plugin").on_attach(function(client, bufnr)
-			if client.server_capabilities.documentSymbolProvider then
+			if vim.g.navic_silence and client.server_capabilities.documentSymbolProvider then
 				require("nvim-navic").attach(client, bufnr)
 			end
 		end)
 	end,
 	config = function()
+		vim.g.navic_silence = true
+
 		local symbol_map = {}
 		for key, value in pairs(require("lspkind").symbol_map) do
 			symbol_map[key] = value .. " "
