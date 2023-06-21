758cd50f234827e9d599a9368a04cd4475bcdbc7
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-06-21 20:18:18 +0100

    disable on terminal

--- nvim/lua/plugin/mini_indentscope.lua
+++ nvim/lua/plugin/mini_indentscope.lua
@@ -2,6 +2,17 @@ return {
 	"echasnovski/mini.indentscope",
 	branch = "stable",
 	event = "VeryLazy",
+	init = function()
+		vim.api.nvim_create_autocmd({ "BufEnter" }, {
+			callback = function()
+				if vim.bo.buftype == "terminal" then
+					vim.b.miniindentscope_disable = true
+				else
+					vim.b.miniindentscope_disable = false
+				end
+			end,
+		})
+	end,
 	config = function(_, opts)
 		require("mini.indentscope").setup(opts)
 	end,
