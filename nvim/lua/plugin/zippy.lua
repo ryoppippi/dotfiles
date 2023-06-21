24e727f1b2861e8afb7b60921d82d9b394c4ba70
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-06-21 23:24:24 +0100

    update zippy config

--- nvim/lua/plugin/zippy.lua
+++ nvim/lua/plugin/zippy.lua
@@ -1,5 +1,9 @@
 return {
 	"PatschD/zippy.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"SmiteshP/nvim-navic",
+	},
 	init = function()
 		vim.api.nvim_create_user_command("ZippyInsertPrint", function()
 			require("zippy").insert_print()
