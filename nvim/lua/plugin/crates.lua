8bdb877688847bc7248695fc4ea38a529907741d
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-06-21 20:19:12 +0100

    add crates.nvim

---
+++ nvim/lua/plugin/crates.lua
@@ -0,0 +1,8 @@
+return {
+  "saecki/crates.nvim",
+  event = { "BufRead Cargo.toml" },
+  requires = { { "nvim-lua/plenary.nvim" } },
+  config = function()
+    require("crates").setup()
+  end,
+}
