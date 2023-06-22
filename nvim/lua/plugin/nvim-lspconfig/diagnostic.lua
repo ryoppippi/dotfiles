5840ee6b1b9e90f0d63192432933681ab8c8880e
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-06-22 10:03:47 +0100

    fix format and diagnosis

--- nvim/lua/plugin/nvim-lspconfig/diagnostic.lua
+++ nvim/lua/plugin/nvim-lspconfig/diagnostic.lua
@@ -4,6 +4,13 @@ end
 
 return {
 	on_attach = function(client, bufnr)
+		-- setup diagnostic signs
+		local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+		end
+
 		vim.diagnostic.config({
 			underline = true,
 			signs = true,
