b62657a1f3da760e833e8058993b9f9033755b7f
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-07-02 15:25:27 +0100

    typescript.lua to vtsls

--- nvim/lua/plugin/nvim-vtsls.lua
+++ nvim/lua/plugin/nvim-vtsls.lua
@@ -3,28 +3,39 @@ return {
 	dependencies = {
 		"neovim/nvim-lspconfig",
 	},
-	enabled = false,
+	enabled = true,
 	event = { "BufReadPre", "BufNewFile" },
 	config = function()
-		local opts = require("lazy.core.config").plugins["nvim-lspconfig"].opts()
+		local commands = require("vtsls").commands
+		local lspconfig_opts = require("lazy.core.config").plugins["nvim-lspconfig"].opts()
+		local inlayHints = lspconfig_opts.typescriptInlayHints
 
 		require("lspconfig.configs").vtsls = require("vtsls").lspconfig
 		require("lspconfig.configs")
 		require("lspconfig").vtsls.setup({
-			capabilities = opts.capabilities,
+			on_attach = function(client, buffer)
+				vim.keymap.set("n", "<leader>to", function()
+					commands.add_missing_imports()
+					commands.remove_unused_imports()
+					commands.organize_imports()
+				end, { desc = "Organize imports", buffer = buffer })
+
+				lspconfig_opts.disable_formatting(client, buffer)
+			end,
+			capabilities = lspconfig_opts.capabilities,
 			single_file_support = false,
 			settings = {
 				typescript = {
 					suggest = {
 						completionFunctionCalls = true,
 					},
-					inlayHints = opts.typescriptInlayHints,
+					inlayHints = inlayHints,
 					tsserver = {
 						pluginPaths = { "." },
 					},
 				},
 				javascript = {
-					inlayHints = opts.typescriptInlayHints,
+					inlayHints = inlayHints,
 				},
 			},
 		})
--- nvim/lua/plugin/typescript-tools.lua
+++ nvim/lua/plugin/typescript-tools.lua
@@ -1,22 +1,24 @@
 return {
 	"pmizio/typescript-tools.nvim",
-	event = { "BufReadPost" },
+	event = { "BufReadPre", "BufNewFile" },
 	dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 	enabled = false,
-	opts = {
-		settings = {
-			tsserver_plugins = { "typescript-styled-plugin", "typescript-svelte-plugin" },
-			tsserver_file_preferences = {
-				quotePreference = "auto",
-				includeCompletionsForModuleExports = true,
-				includeInlayEnumMemberValueHints = true,
-				includeInlayFunctionLikeReturnTypeHints = true,
-				includeInlayFunctionParameterTypeHints = true,
-				includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
-				includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-				includeInlayPropertyDeclarationTypeHints = true,
-				includeInlayVariableTypeHints = true,
+	opts = function()
+		local lspfoncig_opts = require("lazy.core.config").plugins["nvim-lspconfig"].opts()
+		local tih = lspfoncig_opts.typescriptInlayHints
+		return {
+			settings = {
+				tsserver_plugins = { "typescript-styled-plugin", "typescript-svelte-plugin" },
+				tsserver_file_preferences = {
+					includeInlayEnumMemberValueHints = tih.enumMemberValues.enabled,
+					includeInlayFunctionLikeReturnTypeHints = tih.functionLikeReturnTypes.enabled,
+					includeInlayFunctionParameterTypeHints = tih.parameterTypes.enabled,
+					includeInlayParameterNameHints = tih.parameterNames.enabled,
+					includeInlayParameterNameHintsWhenArgumentMatchesName = tih.parameterNames.suppressWhenArgumentMatchesName,
+					includeInlayPropertyDeclarationTypeHints = tih.propertyDeclarationTypes.enabled,
+					includeInlayVariableTypeHints = tih.variableTypes.enabled,
+				},
 			},
-		},
-	},
+		}
+	end,
 }
