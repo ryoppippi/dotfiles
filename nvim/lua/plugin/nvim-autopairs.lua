df93fee7251037b9cbf9b498e135ee1a20831e5e
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-07-07 22:53:35 +0100

    back to insx

--- nvim/lua/plugin/nvim-autopairs.lua
+++ nvim/lua/plugin/nvim-autopairs.lua
@@ -1,6 +1,6 @@
 return {
 	"windwp/nvim-autopairs",
-	-- event = { "InsertEnter" },
+	event = { "InsertEnter" },
 	enabled = false,
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
 	config = {
