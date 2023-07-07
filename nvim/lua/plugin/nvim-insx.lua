c8105ca725eea3e7fdb4dd2c2b01e86cfba55164
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-07-07 22:53:35 +0100

    back to insx

--- nvim/lua/plugin/nvim-insx.lua
+++ nvim/lua/plugin/nvim-insx.lua
@@ -1,44 +1,43 @@
 return {
 	"hrsh7th/nvim-insx",
 	event = { "InsertEnter", "CmdlineEnter" },
-	enabled = true,
 	config = function()
 		-- require("insx.preset.standard").setup({ cmdline = { enabled = true } })
 		local insx = require("insx")
 		local esc = require("insx.helper.regex").esc
 		local helper = require("insx.helper")
 
-		local function auto_tag()
-			local search = {}
-			search.Tag = {
-				Open = [=[<\(\w\+\)\%(\s\+.\{-}\)\?]=],
-				Close = [=[</\w\+>]=],
-			}
-			return {
-				builtin = {
-					["html"] = {
-						{
-							action = function(ctx)
-								local name = ctx.before():match("<(%w+)")
-								local row, col = ctx.row(), ctx.col()
-								ctx.move(row, col + 1)
-								ctx.send(([[</%s>]]):format(name))
-								ctx.move(row, col + 1)
-							end,
-							enabled = function(ctx)
-								return helper.regex.match(ctx.before(), search.Tag.Open) ~= nil
-									and helper.regex.match(ctx.after(), helper.search.Tag.Close) == nil
-							end,
-						},
-					},
-				},
-			}
-		end
+		-- local function auto_tag()
+		-- 	local search = {}
+		-- 	search.Tag = {
+		-- 		Open = [=[<\(\w\+\)\%(\s\+.\{-}\)\?]=],
+		-- 		Close = [=[</\w\+>]=],
+		-- 	}
+		-- 	return {
+		-- 		builtin = {
+		-- 			["html"] = {
+		-- 				{
+		-- 					action = function(ctx)
+		-- 						local name = ctx.before():match("<(%w+)")
+		-- 						local row, col = ctx.row(), ctx.col()
+		-- 						ctx.move(row, col + 1)
+		-- 						ctx.send(([[</%s>]]):format(name))
+		-- 						ctx.move(row, col + 1)
+		-- 					end,
+		-- 					enabled = function(ctx)
+		-- 						return helper.regex.match(ctx.before(), search.Tag.Open) ~= nil
+		-- 							and helper.regex.match(ctx.after(), helper.search.Tag.Close) == nil
+		-- 					end,
+		-- 				},
+		-- 			},
+		-- 		},
+		-- 	}
+		-- end
 
 		-- endwise
 		local endwise = require("insx.recipe.endwise")
 		insx.add("<CR>", endwise(endwise.builtin))
-		insx.add(">", endwise(auto_tag().builtin))
+		-- insx.add(">", endwise(auto_tag().builtin))
 
 		-- quotes
 		for _, quote in ipairs({ '"', "`" }) do
