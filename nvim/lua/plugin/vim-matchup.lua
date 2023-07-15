c5248b7b6814e4574974e292e81f16e6fb1998b5
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-07-15 11:43:41 +0100

    matchup delay -> 0

--- nvim/lua/plugin/vim-matchup.lua
+++ nvim/lua/plugin/vim-matchup.lua
@@ -2,9 +2,10 @@ return {
 	"andymass/vim-matchup",
 	event = "BufReadPost",
 	init = function()
+		vim.g.matchup_matchparen_enabled = 1
 		vim.g.matchup_text_obj_enabled = 1
 		vim.g.matchup_matchparen_deferred = 1
-		vim.g.matchup_matchparen_deferred_show_delay = 300
+		vim.g.matchup_matchparen_deferred_show_delay = 0
 		vim.g.matchup_matchparen_deferred_hide_delay = 700
 		vim.g.matchup_matchparen_timeout = 300
 		vim.g.matchup_matchparen_insert_timeout = 60
