c63b46fcf2023cd42a209f673d43e014d33ad09c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-07-15 22:58:12 +0100

    disable vim-expand-region

--- nvim/lua/plugin/vim-expand-region.lua
+++ nvim/lua/plugin/vim-expand-region.lua
@@ -1,5 +1,6 @@
 return {
 	"terryma/vim-expand-region",
+	enabled = false,
 	keys = {
 		{ "v", "<Plug>(expand_region_expand)", mode = { "n", "v" } },
 		{ "<c-v>", "<Plug>(expand_region_shrink)", mode = { "v" } },
