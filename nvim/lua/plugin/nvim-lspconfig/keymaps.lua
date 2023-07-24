38d3dabb0692a91ebd46d78d553cec707f215e6f
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-07-24 18:30:56 +0100

    fix keymaps

--- nvim/lua/plugin/nvim-lspconfig/keymaps.lua
+++ nvim/lua/plugin/nvim-lspconfig/keymaps.lua
@@ -1,39 +1,77 @@
 local has = require("core.plugin").has
 return {
 	on_attach = function(_, bufnr)
-		local opts = { silent = true, buffer = bufnr }
+		local function opts(desc)
+			return { silent = true, buffer = bufnr, desc = desc }
+		end
+
 		local has_telescope = has("telescope.nvim")
+		local has_glance = has("glance.nvim")
 		local has_action_preview = has("actions-preview.nvim")
 
 		-- hover doc
-		vim.keymap.set("n", "gh", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+		vim.keymap.set("n", "gh", "<cmd>lua vim.lsp.buf.hover()<cr>", opts("hover doc"))
 
 		-- jump to
-		vim.keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+		vim.keymap.set("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<cr>", opts("jump to declaration"))
 
-		if has_telescope then
-			vim.keymap.set("n", "gd", [[<cmd>Telescope lsp_definitions<cr>]], opts)
-			vim.keymap.set("n", "gt", [[<cmd>Telescope lsp_type_definitions<cr>]], opts)
-			vim.keymap.set("n", "gI", [[<cmd>Telescope lsp_implementations<cr>]], opts)
-			vim.keymap.set("n", "gr", [[<cmd>Telescope lsp_references<cr>]], opts)
-		else
-			vim.keymap.set("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-			vim.keymap.set("n", "gI", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-			vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-		end
+		vim.keymap.set("n", "gd", function()
+			if has_glance then
+				vim.cmd([[Glance definitions]])
+				return
+			end
+			if has_telescope then
+				vim.cmd([[Telescope lsp_definitions]])
+				return
+			end
+			vim.lsp.buf.type_definition()
+		end, opts("jump to definition"))
+		vim.keymap.set("n", "gt", function()
+			if has_glance then
+				vim.cmd([[Glance type_definitions]])
+				return
+			end
+			if has_telescope then
+				vim.cmd([[Telescope lsp_type_definitions]])
+				return
+			end
+			vim.lsp.buf.type_definition()
+		end, opts("jump to type_definition"))
+		vim.keymap.set("n", "gI", function()
+			if has_glance then
+				vim.cmd([[Glance implementations]])
+				return
+			end
+			if has_telescope then
+				vim.cmd([[Telescope lsp_implementations]])
+				return
+			end
+			vim.lsp.buf.implementation()
+		end, opts("jump to implementation"))
+		vim.keymap.set("n", "gr", function()
+			if has_glance then
+				vim.cmd([[Glance references]])
+				return
+			end
+			if has_telescope then
+				vim.cmd([[Telescope lsp_references]])
+				return
+			end
+			vim.lsp.buf.references()
+		end, opts("jump to references"))
 
 		-- signature_help
-		vim.keymap.set("i", "<C-k>", "<cmd>vim.lsp.buf.signature_help()<cr>", opts)
-		vim.keymap.set("n", "gk", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+		vim.keymap.set("i", "<C-k>", "<cmd>vim.lsp.buf.signature_help()<cr>", opts("signature_help"))
+		vim.keymap.set("n", "gk", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts("signature_help"))
 
 		-- diagnostics
-		vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
+		vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>", opts("open diagnostics float"))
 		if has_telescope then
-			vim.keymap.set("n", "gL", [[<cmd>Telescope diagnostics<cr>]], opts)
+			vim.keymap.set("n", "gL", [[<cmd>Telescope diagnostics<cr>]], opts("open diagnostics telescope"))
 		end
 
-		vim.keymap.set("n", "-", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
-		vim.keymap.set("n", "_", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
+		vim.keymap.set("n", "-", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts("goto next diagnostic"))
+		vim.keymap.set("n", "_", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts("goto prev diagnostic"))
 
 		-- rename
 		vim.keymap.set("n", "cr", function()
@@ -45,13 +83,17 @@ return {
 		end, { expr = true, buffer = bufnr, desc = "rename words" })
 
 		-- code actions
-		if not has_action_preview then
-			vim.keymap.set({ "n", "v", "x" }, "ga", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-		end
+		vim.keymap.set({ "n", "v", "x" }, "ga", function()
+			if has_action_preview then
+				require("actions-preview").code_actions()
+				return
+			end
+			vim.lsp.buf.code_action()
+		end, opts("code_action"))
 
 		-- workspace
-		vim.keymap.set("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>", opts)
-		vim.keymap.set("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>", opts)
-		vim.keymap.set("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>", opts)
+		vim.keymap.set("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>", opts("add workspace folder"))
+		vim.keymap.set("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>", opts("remove workspace folder"))
+		vim.keymap.set("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>", opts("list workspace folders"))
 	end,
 }
