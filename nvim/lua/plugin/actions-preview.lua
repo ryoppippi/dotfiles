ba7e894057885d1f6322cd5ddc75dca6bb428dd0
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-07-24 18:30:56 +0100

    fix keymaps

--- nvim/lua/plugin/actions-preview.lua
+++ nvim/lua/plugin/actions-preview.lua
@@ -4,17 +4,6 @@ return {
 		"neovim/nvim-lspconfig",
 		"nvim-telescope/telescope.nvim",
 	},
-	event = "LspAttach",
-	init = function()
-		require("core.plugin").on_attach(function(_, buffer)
-			vim.keymap.set(
-				{ "n", "v", "x" },
-				"ga",
-				require("actions-preview").code_actions,
-				{ silent = true, buffer = buffer, desc = "code actions preview" }
-			)
-		end)
-	end,
 	opts = function()
 		return {
 			telescope = require("telescope.themes").get_dropdown({ winblend = 10 }),
