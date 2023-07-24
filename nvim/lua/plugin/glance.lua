db74ac167db78639806a00d14b256dd2b7608af8
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-07-24 22:31:50 +0100

    update config for glance.nvim

--- nvim/lua/plugin/glance.lua
+++ nvim/lua/plugin/glance.lua
@@ -13,6 +13,22 @@ return {
 					["<C-v>"] = glance.actions.jump_vsplit,
 				},
 			},
+			hooks = {
+				before_open = function(results, open, jump, method)
+					local uri = vim.uri_from_bufnr(0)
+					if #results == 1 then
+						local target_uri = results[1].uri or results[1].targetUri
+
+						if target_uri == uri then
+							jump(results[1])
+						else
+							open(results)
+						end
+					else
+						open(results)
+					end
+				end,
+			},
 		}
 	end,
 	config = true,
