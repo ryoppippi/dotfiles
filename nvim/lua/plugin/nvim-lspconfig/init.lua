8095209e1d72384efe1248c4e708fb09c382a886
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-07-26 11:15:14 +0100

    add openapi schema

--- nvim/lua/plugin/nvim-lspconfig/init.lua
+++ nvim/lua/plugin/nvim-lspconfig/init.lua
@@ -222,7 +222,19 @@ return {
 		setup(lspconfig.yamlls, {
 			settings = {
 				yaml = {
-					schemas = require("schemastore").yaml.schemas(),
+					schemas = require("schemastore").yaml.schemas({
+						extra = {
+							{
+								name = "openAPI 3.0",
+								url = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.0/schema.yaml",
+							},
+							{
+
+								name = "openAPI 3.1",
+								url = "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.yaml",
+							},
+						},
+					}),
 					validate = true,
 					format = { enable = true },
 				},
