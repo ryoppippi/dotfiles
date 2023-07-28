ce488d6b170b84c2ccdb7b99a7ae7975b1cd92a2
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-07-28 21:19:41 +0100

    update lightbulb config

--- nvim/lua/plugin/nvim-lightbulb.lua
+++ nvim/lua/plugin/nvim-lightbulb.lua
@@ -1,7 +1,11 @@
 return {
 	"kosayoda/nvim-lightbulb",
 	event = { "LspAttach" },
-	config = function()
-		require("nvim-lightbulb").setup({ autocmd = { enabled = true } })
+	opts = function()
+		return {
+			autocmd = { enabled = true },
+			sign = { enabled = false },
+			virtual_text = { enabled = true },
+		}
 	end,
 }
