feecd98a577d6ad972b67eac1711ed3ee4d5e0fc
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-08-01 20:15:06 +0100

    update committia.vim config

--- nvim/lua/plugin/committia.lua
+++ nvim/lua/plugin/committia.lua
@@ -1,6 +1,11 @@
 return {
 	"rhysd/committia.vim",
-	ft = {
-		"git",
-	},
+	dependencies = { "tani/vim-artemis" },
+	ft = { "gitcommit" },
+	config = function()
+		local bufname = vim.fs.basename(vim.api.nvim_buf_get_name(0))
+		if bufname == "COMMIT_EDITMSG" or bufname == "MERGE_MSG" then
+			vimx.fn.committia.open("git")
+		end
+	end,
 }
