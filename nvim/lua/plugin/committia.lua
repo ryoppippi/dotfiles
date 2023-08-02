b2a03e48bb83de3b7e44d86cad578a56c8a96bfd
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-08-02 16:25:38 +0100

    disable commitia

--- nvim/lua/plugin/committia.lua
+++ nvim/lua/plugin/committia.lua
@@ -1,8 +1,16 @@
 return {
 	"rhysd/committia.vim",
+	enabled = false,
 	dependencies = { "tani/vim-artemis" },
+	event = "VeryLazy",
 	ft = { "gitcommit" },
 	config = function()
+		-- vim.g.committia_hooks = {
+		-- 	edit_open = function(info)
+		-- 		if info.vcs == "git" and vim.fn.getline(1) == "" then
+		-- 		end
+		-- 	end,
+		-- }
 		local bufname = vim.fs.basename(vim.api.nvim_buf_get_name(0))
 		if bufname == "COMMIT_EDITMSG" or bufname == "MERGE_MSG" then
 			vimx.fn.committia.open("git")
