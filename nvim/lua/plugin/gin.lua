42729345bd2f2a3bedf548216f008f878917ab7e
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-08-02 19:50:23 +0100

    enable gin

--- nvim/lua/plugin/gin.lua
+++ nvim/lua/plugin/gin.lua
@@ -1,7 +1,6 @@
 return {
 	"lambdalisue/gin.vim",
 	event = "VeryLazy",
-	enabled = false,
 	dependencies = { "vim-denops/denops.vim" },
 	config = function()
 		require("denops-lazy").load("gin.vim")
