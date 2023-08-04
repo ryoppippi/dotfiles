cece0cb8e3b5610a65a05d2203eecfb881d2888d
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-08-04 22:41:26 +0100

    fix telescope config

--- nvim/lua/core/plugin.lua
+++ nvim/lua/core/plugin.lua
@@ -26,6 +26,27 @@ function M.on_attach(on_attach)
 	})
 end
 
+---@param name string
+---@param fn fun(name:string)
+function M.on_load(name, fn)
+	local Config = require("lazy.core.config")
+	if Config.plugins[name] and Config.plugins[name]._.loaded then
+		vim.schedule(function()
+			fn(name)
+		end)
+	else
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "LazyLoad",
+			callback = function(event)
+				if event.data == name then
+					fn(name)
+					return true
+				end
+			end,
+		})
+	end
+end
+
 ---@param plugin string
 function M.has(plugin)
 	return require("lazy.core.config").spec.plugins[plugin] ~= nil
