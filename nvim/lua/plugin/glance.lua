bcdd34b5b346fc9fb9be2dc33d3aa07fd766a22c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-08-05 10:30:38 +0100

    delete unused keymaps

--- nvim/lua/plugin/glance.lua
+++ nvim/lua/plugin/glance.lua
@@ -2,17 +2,10 @@ return {
 	"dnlhc/glance.nvim",
 	cmd = { "Glance" },
 	opts = function()
-		local glance = require("glance")
 		return {
 			border = { enable = true },
 			list = { position = "left" },
 			folds = { folded = false },
-			mappings = {
-				list = {
-					["<C-s>"] = glance.actions.jump_split,
-					["<C-v>"] = glance.actions.jump_vsplit,
-				},
-			},
 			hooks = {
 				before_open = function(results, open, jump, method)
 					local uri = vim.uri_from_bufnr(0)
