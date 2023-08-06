b8f6f79602438ad0bac424c491dc1be59daae352
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-08-06 20:44:55 +0100

    dashboard-nvim: add plugin

---
+++ nvim/lua/plugin/dashboard-nvim.lua
@@ -0,0 +1,32 @@
+return {
+	"glepnir/dashboard-nvim",
+	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+	event = "VimEnter",
+	opts = function()
+		return {
+			theme = "hyper",
+			config = {
+				week_header = {
+					enable = true,
+				},
+				shortcut = {
+					{ desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
+					{
+						desc = "Files",
+						icon = " ",
+						icon_hl = "@variable",
+						group = "Label",
+						action = "Telescope find_files",
+						key = "f",
+					},
+					{
+						desc = "  MRU",
+						group = "DiagnosticHint",
+						action = "Telescope oldfiles",
+						key = "r",
+					},
+				},
+			},
+		}
+	end,
+}
