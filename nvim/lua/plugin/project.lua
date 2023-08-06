c8ea46c521491dcb294238cc63e702f4272ecb9e
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-08-06 18:43:07 +0100

    priject.nvim: fix root patterns

--- nvim/lua/plugin/project.lua
+++ nvim/lua/plugin/project.lua
@@ -1,6 +1,37 @@
 return {
 	"ahmedkhalf/project.nvim",
-	event = "BufWinEnter",
+	event = "VeryLazy",
+	init = function()
+		require("plugin.telescope").le("projects")
+	end,
+	opts = {
+		patterns = {
+			-- git
+			".git",
+			-- shell
+			"Makefile",
+			-- node
+			"package.json",
+			"tsconfig.json",
+			"node_modules",
+			-- deno
+			"deno.json",
+			"deno.jsonc",
+			"import_map.json",
+			-- lua
+			"stylua.toml",
+			".stylua.toml",
+			-- go
+			"go.mod",
+			-- rust
+			"Cargo.toml",
+			-- python
+			"pyproject.toml",
+			".venv",
+			-- zig
+			"build.zig",
+		},
+	},
 	config = function(_, opts)
 		require("project_nvim").setup(opts)
 	end,
