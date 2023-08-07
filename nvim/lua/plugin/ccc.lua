456d4e8f01a208e1bc9e5799c55991f64e2edd06
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-08-07 21:04:54 +0100

    ccc.nvim: fix options

--- nvim/lua/plugin/ccc.lua
+++ nvim/lua/plugin/ccc.lua
@@ -1,17 +1,11 @@
-local event = { "ColorScheme", "BufReadPost" }
 return {
 	"uga-rosa/ccc.nvim",
 	cond = vim.o.termguicolors and not is_vscode(),
-	event = event,
-	config = function()
-		require("ccc").setup({})
-		vim.api.nvim_create_autocmd(event, {
-			pattern = "*",
-			callback = function()
-				require("ccc").setup({})
-				vim.cmd([[CccHighlighterDisable]])
-				vim.cmd([[CccHighlighterEnable]])
-			end,
-		})
-	end,
+	event = { "BufReadPost" },
+	opts={
+		highlighter={
+			auto_enable=true,
+			lsp=true,
+		}
+	}
 }
