2629339e92cc9b988388b98cbfe44e75b01df050
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-08-08 13:40:12 +0100

    format code

--- nvim/lua/plugin/ccc.lua
+++ nvim/lua/plugin/ccc.lua
@@ -2,10 +2,10 @@ return {
 	"uga-rosa/ccc.nvim",
 	cond = vim.o.termguicolors and not is_vscode(),
 	event = { "BufReadPost" },
-	opts={
-		highlighter={
-			auto_enable=true,
-			lsp=true,
-		}
-	}
+	opts = {
+		highlighter = {
+			auto_enable = true,
+			lsp = true,
+		},
+	},
 }
