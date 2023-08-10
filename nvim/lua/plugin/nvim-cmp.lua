14c24ea282575346c9b9923d6519956f7d5b842c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-08-10 08:09:06 +0100

    nvim-cmp: fix `sorting.comparators`

--- nvim/lua/plugin/nvim-cmp.lua
+++ nvim/lua/plugin/nvim-cmp.lua
@@ -138,26 +138,7 @@ return {
 					cmp.config.compare.exact,
 					cmp.config.compare.score,
 					require("cmp-under-comparator").under,
-					function(entry1, entry2)
-						local kind1 = entry1:get_kind()
-						kind1 = kind1 == types.lsp.CompletionItemKind.Text and 100 or kind1
-						local kind2 = entry2:get_kind()
-						kind2 = kind2 == types.lsp.CompletionItemKind.Text and 100 or kind2
-						if kind1 ~= kind2 then
-							if kind1 == types.lsp.CompletionItemKind.Snippet then
-								return false
-							end
-							if kind2 == types.lsp.CompletionItemKind.Snippet then
-								return true
-							end
-							local diff = kind1 - kind2
-							if diff < 0 then
-								return true
-							elseif diff > 0 then
-								return false
-							end
-						end
-					end,
+					cmp.config.compare.kind,
 					cmp.config.compare.sort_text,
 					cmp.config.compare.length,
 					cmp.config.compare.order,
