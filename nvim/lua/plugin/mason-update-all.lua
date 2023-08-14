34a3e8d5324e8a08632c2ca161a567895daa3cb2
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-08-14 17:05:48 +0100

    mason-update-all: fix config

--- nvim/lua/plugin/mason-update-all.lua
+++ nvim/lua/plugin/mason-update-all.lua
@@ -1,16 +1,18 @@
 return {
 	"RubixDev/mason-update-all",
 	cond = not is_vscode(),
+	cmd = { "MasonUpdateAll" },
 	dependencies = {
 		"williamboman/mason.nvim",
 	},
-	config = function()
+	config = true,
+	init = function()
 		vim.api.nvim_create_autocmd("User", {
 			pattern = "LazyUpdate",
 			callback = function()
 				require("mason-update-all").update_all()
 			end,
-			desc = "Run mason-update-all after packer.sync()",
+			desc = "Run mason-update-all after LazyUpdate",
 		})
 	end,
 }
