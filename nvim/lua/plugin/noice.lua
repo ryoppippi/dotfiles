6f479cb179e4a1c3f819ead92587c1211eb369c7
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-08-20 21:23:17 +0100

    noice.nvim: fix options for cmp conditions

--- nvim/lua/plugin/noice.lua
+++ nvim/lua/plugin/noice.lua
@@ -1,3 +1,4 @@
+local has = require("core.plugin").has
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
@@ -10,35 +11,37 @@ return {
 	init = function()
 		require("plugin.telescope").le("noice")
 	end,
-	opts = {
-		lsp = {
-			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true,
+	opts = function()
+		return {
+			lsp = {
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
 			},
-		},
-		popupmenu = {
-			enabled = true,
-			backend = "cmp",
-		},
-		notify = {
-			enabled = true,
-			view = "mini",
-		},
-		cmdline = {
-			enabled = true,
-		},
-		messages = {
-			view_search = "virtualtext",
-			view = "mini",
-		},
-		presets = {
-			inc_rename = true,
-			lsp_doc_border = true,
-			long_message_to_split = true,
-		},
-	},
+			popupmenu = {
+				enabled = true,
+				backend = has("nvim-cmp") and "cmp" or "nui",
+			},
+			notify = {
+				enabled = true,
+				view = "mini",
+			},
+			cmdline = {
+				enabled = true,
+			},
+			messages = {
+				view_search = "virtualtext",
+				view = "mini",
+			},
+			presets = {
+				inc_rename = true,
+				lsp_doc_border = true,
+				long_message_to_split = true,
+			},
+		}
+	end,
 	config = true,
 }
