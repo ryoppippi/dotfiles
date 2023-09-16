25e13f378a4347e6fd8d3540f53ea74a93e6229e
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-09-16 12:56:16 +0100

    fish: fix rm.fish

--- fish/user_functions/rm.fish
+++ fish/user_functions/rm.fish
@@ -1,4 +1,6 @@
 function rm
     type -q trash && trash $argv && return
+    type -q bun && bun x trash $argv && return
+    type -q npx && npx trash-cli $argv && return
     type -q /bin/rm && /bin/rm $argv && return
 end
