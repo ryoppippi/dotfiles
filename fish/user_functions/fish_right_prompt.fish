e3a3b16045e6eaf4c9a172424b9d85765699baf1
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-09-16 20:08:03 +0100

    fish: implement `franciscolourenco/done` by own

---
+++ fish/user_functions/fish_right_prompt.fish
@@ -0,0 +1,13 @@
+set -gx NOTIFY_ON_COMMAND_DURATION 5000
+function fish_right_prompt
+    if test $CMD_DURATION
+        # Show notification if dration is more than 30 seconds
+        if test $CMD_DURATION -gt $NOTIFY_ON_COMMAND_DURATION
+            # Show duration of the last command in seconds
+            set duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
+            set -f MSG (echo (history | head -1) returned $status after $duration)
+            type -q notify-send && notify-send $MSG && return
+            type -q terminal-notifier && terminal-notifier -message $MSG && return
+        end
+    end
+end
