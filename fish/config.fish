c9fa0ca15544456bd1dac67b19628e2156020168
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-09-17 15:50:21 +0100

    fish: fix AQUA_GLOBAL_CONFIG

--- fish/config.fish
+++ fish/config.fish
@@ -39,7 +39,7 @@ fish_add_path $HOMEBREW_X86_64/bin
 set -gx AQUA_ROOT_DIR $XDG_DATA_HOME/aquaproj-aqua
 fish_add_path -p $AQUA_ROOT_DIR/bin
 
-set -gx AQUA_GLOBAL_CONFIG $AQUA_GLOBAL_CONFIG $XDG_CONFIG_HOME/aquaproj-aqua/aqua.yaml
+set -gx AQUA_GLOBAL_CONFIG $XDG_CONFIG_HOME/aquaproj-aqua/aqua.yaml
 
 # c / c++
 # c++
