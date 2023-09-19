1c34cac6df5083973e51a1fee0e787228b39bf72
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-09-19 15:23:05 +0100

    fish: create ls function

--- fish/config/abbrs_aliases.fish
+++ fish/config/abbrs_aliases.fish
@@ -3,13 +3,10 @@ set -x LOADED_ABBRS_ALIASES 1
 
 alias vim nvim
 alias bash 'bash --norc'
-abbr ls lsd
-if type -q lsd
-    abbr ll 'lsd -hlF'
-    abbr la 'lsd -hlFA'
-    abbr lt 'lsd --tree'
-    abbr lg 'lsd -hlFg'
-end
+abbr ll 'ls -hlF'
+abbr la 'ls -hlFA'
+abbr lt 'ls --tree'
+abbr lg 'ls -hlFg'
 abbr -a cdr 'cd (git root)'
 abbr -a sed gsed
 abbr -a sc "source $FISH_CONFIG"
