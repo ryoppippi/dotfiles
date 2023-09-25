20b6608991fc4e2d7dc4a532fd3d09a4e9286a32
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-09-25 17:33:15 +0100

    nvim-lspconfig: fix denols root_dir

--- nvim/lua/plugin/nvim-lspconfig/init.lua
+++ nvim/lua/plugin/nvim-lspconfig/init.lua
@@ -213,6 +213,7 @@ return {
 
 		setup(lspconfig.denols, {
 			single_file_support = true,
+			root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
 			on_attach = function(client, bufnr)
 				local auto_disable_servers = {
 					"vtsls",
