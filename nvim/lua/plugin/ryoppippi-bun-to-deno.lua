d17a6fab20fb8684ca3cf5bcc356514f62b51914
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-09-26 12:06:49 +0100

    update setup.sh

---
+++ nvim/lua/plugin/ryoppippi-bun-to-deno.lua
@@ -0,0 +1,37 @@
+return {
+	dir = "",
+	name = "ryoppippi/bun-to-deno",
+	init = function()
+		vim.api.nvim_create_user_command("BunToDeno", function()
+			-- get current buffer text
+			local text = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+			-- loop by lines
+			for i, line in ipairs(text) do
+				-- if the line stats with `import`
+				if string.match(line, "^import") then
+					-- import { hoge } from "hoge" => import { hoge } from "npm:hoge"
+					-- import { hoge } from 'hoge' => import { hoge } from 'npm:hoge'
+					text[i] = string.gsub(line, '"([^"]+)"', '"npm:%1"')
+				end
+			end
+			-- set replaced text to current buffer
+			vim.api.nvim_buf_set_lines(0, 0, -1, true, text)
+		end, {})
+
+		vim.api.nvim_create_user_command("DenoToBun", function()
+			-- get current buffer text
+			local text = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+			-- loop by lines
+			for i, line in ipairs(text) do
+				-- if the line stats with `import`
+				if string.match(line, "^import") then
+					-- import { hoge } from "npm:hoge" => import { hoge } from "hoge"
+					-- import { hoge } from 'npm:hoge' => import { hoge } from 'hoge'
+					text[i] = string.gsub(line, '"npm:([^"]+)"', '"%1"')
+				end
+			end
+			-- set replaced text to current buffer
+			vim.api.nvim_buf_set_lines(0, 0, -1, true, text)
+		end, {})
+	end,
+}
