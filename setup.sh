a4cbb37d849ea9de3a27b00b874914c2fd918a6e
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-09-26 11:41:17 +0100

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,8 +13,20 @@ XDG_CONFIG_HOME=$HOME/.config
 XDG_CACHE_HOME=$HOME/.cache
 XDG_DATA_HOME=$HOME/.local/share
 
+
+if [ "$(uname)" == "Darwin" ] ; then
+  xcode-select --install
+fi
+
 # aqua
 curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.0.2/aqua-installer | bash
+
+# install deps via aqua
+export PATH="${AQUA_ROOT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/aquaproj-aqua}/bin:$PATH"
+
+cd $DIR/aqua \
+  && aqua install -l -a \
+  && dotfiles link .
 
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -27,18 +39,10 @@ export HOMEBREW_BREWFILE="$HOME/.brewfile"
 brew bundle --global
 brew reinstall fish
 
-# aqua
-curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.0.2/aqua-installer | bash
-
-
 # fish shell
 fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
 fish -c "fisher update"
 fish -c "fish_update_completions"
-
-# install deps via aqua
-export PATH="${AQUA_ROOT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/aquaproj-aqua}/bin:$PATH"
-cd $DIR/aqua && aqua install -l -a
 
 cd $DIR
 
@@ -50,4 +54,3 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # post install
 brew cleanup -s
-
