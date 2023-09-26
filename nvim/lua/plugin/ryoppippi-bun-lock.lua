0aee11f4fd93a757ed26e32bd582033665b13301
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-09-26 19:41:17 +0100

    bun-lock:stop using yarn file

--- nvim/lua/plugin/ryoppippi-bun-lock.lua
+++ nvim/lua/plugin/ryoppippi-bun-lock.lua
@@ -5,28 +5,18 @@ return {
 		vim.api.nvim_create_autocmd("BufReadCmd", {
 			pattern = "bun.lockb",
 			callback = function()
-				-- get directory of the current buffer
-				local dir = vim.fn.expand("%:p:h")
-				-- if yarn.lock exists, rename to yarn_tmp.lock
-				if vim.fn.filereadable(dir .. "/yarn.lock") == 1 then
-					vim.fn.rename(dir .. "/yarn.lock", dir .. "/yarn_tmp.lock")
-				end
-				-- excecute command `bun install --dry-run --yarn`
-				vim.fn.system("bun install --dry-run --yarn", dir)
-				-- read a yarn.lock and write to the buffer
-				vim.api.nvim_buf_set_lines(0, 0, -1, true, vim.fn.readfile(dir .. "/yarn.lock"))
+				-- get the absolute path of the current file
+				local path = vim.fn.expand("%:p")
+				-- run command 'bun ' .. path and get stdout
+				local output = vim.fn.systemlist("bun " .. path)
+				-- set output to current buffer
+				vim.api.nvim_buf_set_lines(0, 0, -1, true, output)
 				-- set filetype to yarn.lock
 				vim.opt_local.filetype = "conf"
 				-- set readonly
 				vim.opt_local.readonly = true
 				-- set nomodifiable
 				vim.opt_local.modifiable = false
-				-- delete yarn.lock
-				vim.fn.delete(dir .. "/yarn.lock")
-				-- if yarn_tmp.lock exists, rename to yarn.lock
-				if vim.fn.filereadable(dir .. "/yarn_tmp.lock") == 1 then
-					vim.fn.rename(dir .. "/yarn_tmp.lock", dir .. "/yarn.lock")
-				end
 			end,
 		})
 	end,
