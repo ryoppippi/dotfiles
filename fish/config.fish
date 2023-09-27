65d9279cd85f0980dc772843627c075fdd929e6c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-09-27 16:37:26 +0100

    fish: add AQUA_GLOBAL_CONFIG

--- fish/config.fish
+++ fish/config.fish
@@ -37,6 +37,7 @@ fish_add_path $HOMEBREW_X86_64/bin
 
 # aqua
 set -gx AQUA_ROOT_DIR $XDG_DATA_HOME/aquaproj-aqua
+set -gx AQUA_GLOBAL_CONFIG $XDG_CONFIG_HOME/aquaproj-aqua/aqua.yaml
 fish_add_path -p $AQUA_ROOT_DIR/bin
 
 set -gx AQUA_GLOBAL_CONFIG $XDG_CONFIG_HOME/aquaproj-aqua/aqua.yaml
