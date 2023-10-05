29afc296aa82b216f40f3a3da75c1f9334082f50
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-10-05 18:18:30 +0100

    fish: delete duplicated aqau_global

--- fish/config.fish
+++ fish/config.fish
@@ -40,8 +40,6 @@ set -gx AQUA_ROOT_DIR $XDG_DATA_HOME/aquaproj-aqua
 set -gx AQUA_GLOBAL_CONFIG $XDG_CONFIG_HOME/aquaproj-aqua/aqua.yaml
 fish_add_path -p $AQUA_ROOT_DIR/bin
 
-set -gx AQUA_GLOBAL_CONFIG $XDG_CONFIG_HOME/aquaproj-aqua/aqua.yaml
-
 # c / c++
 # c++
 # export CPPFLAGS=-I/opt/X11/include
