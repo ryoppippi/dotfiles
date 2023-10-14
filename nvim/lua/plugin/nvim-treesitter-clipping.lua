0d076d88414c80783e431b3adab6a2c586cffcb6
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-10-14 19:40:17 +0100

    nvim-treesitter-clipping: fix keymaps

--- nvim/lua/plugin/nvim-treesitter-clipping.lua
+++ nvim/lua/plugin/nvim-treesitter-clipping.lua
@@ -1,7 +1,21 @@
 return {
 	"monaqa/nvim-treesitter-clipping",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"thinca/vim-partedit",
+	},
 	keys = {
-		{ "<leader>c", "<Plug>(ts-clipping-clip)", { "n" } },
-		{ "<leader>c", "<Plug>(ts-clipping-select)", { "x", "o" } },
+		{
+			"<leader>c",
+			"<Plug>(ts-clipping-clip)",
+			desc = "Clip",
+			mode = { "n" },
+		},
+		{
+			"<leader>c",
+			"<Plug>(ts-clipping-select)",
+			desc = "Clip Select",
+			mode = { "x", "o" },
+		},
 	},
 }
