464fd30c7a5e2888fd854121bb8b9d387e7ff6ee
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-10-17 09:31:05 +0100

    gh: fix install command

--- nvim/lua/cli/gh.lua
+++ nvim/lua/cli/gh.lua
@@ -1,22 +1,15 @@
-local link = function(spec)
+local install = function(spec)
 	local installedDir = spec.dir
-	local name = spec.name
 
-	-- get xdg data dir
-	local xdgDataDir = os.getenv("XDG_DATA_HOME") or os.getenv("HOME") .. "/.local/share"
-	local ghExtensionDir = xdgDataDir .. "/gh/extensions"
-
-	-- vim.uv.fs_mkdir(ghExtensionDir, 511)
-	vim.fn.mkdir(ghExtensionDir, "p")
-	vim.uv.fs_symlink(installedDir, ghExtensionDir .. "/" .. name)
+	vim.system({ "gh", "extension", "install", "." }, { cwd = installedDir, text = true })
 end
 
 return {
-	{ "yusukebe/gh-markdown-preview", build = { "go clean", "go build", link } },
-	{ "dlvhdr/gh-dash", build = { "go clean", "go build", link } },
-	{ "actions/gh-actions-cache", build = { "go clean", "go build", link } },
-	{ "seachicken/gh-poi", build = { "go clean", "go build", link } },
-	{ "k1LoW/gh-do", build = { link } },
-	{ "kawarimidoll/gh-graph", build = { link } },
-	{ "kawarimidoll/gh-q", build = { link } },
+	{ "yusukebe/gh-markdown-preview", build = { "go build", install } },
+	{ "dlvhdr/gh-dash", build = { "go build", install } },
+	{ "actions/gh-actions-cache", build = { "go build", install } },
+	{ "seachicken/gh-poi", build = { "go build", install } },
+	{ "k1LoW/gh-do", build = { install } },
+	{ "kawarimidoll/gh-graph", build = { install } },
+	{ "kawarimidoll/gh-q", build = { install } },
 }
