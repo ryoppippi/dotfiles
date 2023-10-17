e21bd83d9a755a22556d4ec9a38d344094531bfd
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-10-17 19:50:58 +0100

    delete commands from cmdline history like 'q', 'w'
    https://blog.atusy.net/2023/07/24/vim-clean-history/

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -113,3 +113,13 @@ vim.api.nvim_create_autocmd("CmdlineEnter", {
 		end
 	end,
 })
+
+vim.api.nvim_create_autocmd("ModeChanged", {
+	pattern = "c:*",
+	callback = function()
+		local cmd = vim.fn.histget(":", -1)
+		if cmd ~= nil and  (cmd == "x" or cmd == "xa" or cmd:match("^w?q?a?!?$")) then
+			vim.fn.histdel(":", -1)
+		end
+	end,
+})
