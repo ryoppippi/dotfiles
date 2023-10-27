d2226058de65a78ede7c621c6d9d74aa4b779ff1
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-10-27 17:17:57 +0100

    mini.surround: emmet by atusy san

--- nvim/lua/plugin/mini_surround.lua
+++ nvim/lua/plugin/mini_surround.lua
@@ -1,9 +1,90 @@
+-- https://blog.atusy.net/2023/09/01/mini-surround-emmet/
+local lpeg = vim.lpeg
+local name = (lpeg.P(1) - lpeg.S("#.[>")) ^ 1
+local tag = lpeg.Cg(name, "tag")
+local id = lpeg.P("#") * name
+local cls = lpeg.P(".") * name
+local kv = lpeg.P({ "[" * ((1 - lpeg.S("[]")) + lpeg.V(1)) ^ 0 * "]" })
+local child = lpeg.P(">") * lpeg.P(1) ^ 1
+local attr = lpeg.C(child + kv + id + cls) ^ 0
+
+local kindmap = {
+	["."] = "class",
+	["#"] = "id",
+	["["] = "kv",
+	[">"] = "child",
+}
+
+local function parse(s)
+	local splits = lpeg.match(lpeg.Ct(tag * attr), s)
+
+	if type(splits) ~= "table" then
+		return
+	end
+
+	local res = {
+		tag = splits.tag,
+		class = {},
+		id = {},
+		kv = {},
+		child = nil,
+	}
+
+	for _, sp in ipairs(splits) do
+		local k, v = string.match(sp, "(.)(.*)")
+		local kind = kindmap[k]
+		if kind == "child" then
+			res.child = parse(v)
+		else
+			if kind == "kv" then
+				v = string.gsub(v, "%]$", "")
+			end
+			table.insert(res[kind], v)
+		end
+	end
+
+	return res
+end
+
+local function opentag(x)
+	local res = "<" .. x.tag
+	if #x.id > 0 then
+		res = res .. ' id="' .. x.id[1] .. '"'
+	end
+	if #x.class > 0 then
+		res = res .. ' class="' .. table.concat(x.class, " ") .. '"'
+	end
+	if #x.kv > 0 then
+		res = res .. " " .. table.concat(x.kv, " ")
+	end
+	res = res .. ">"
+	if x.child then
+		res = res .. opentag(x.child)
+	end
+	return res
+end
+
+local function closetag(x)
+	local res = ""
+	if x.child then
+		res = closetag(x.child) .. res
+	end
+	res = res .. "</" .. x.tag .. ">"
+	return res
+end
+
+local function totag(x)
+	local p = parse(x)
+	return { left = opentag(p), right = closetag(p) }
+end
+
 return {
 	"echasnovski/mini.surround",
 	version = "*",
 	event = "VeryLazy",
 	enabled = true,
 	opts = {
+		n_lines = 100,
 		mappings = {
 			add = "sa", -- Add surrounding in Normal and Visual modes
 			delete = "sd", -- Delete surrounding
@@ -16,7 +97,6 @@ return {
 			suffix_next = "n", -- Suffix to search with "next" method
 		},
 		custom_surroundings = {
-			-- from https://github.com/atusy/dotfiles/blob/d387c4e1fdacfd971e349eccdd4860505d51c367/dot_config/nvim/lua/plugins/textobj.lua#L146-L199
 			t = {
 				input = { "<(%w-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- from https://github.com/echasnovski/mini.surround/blob/14f418209ecf52d1a8de9d091eb6bd63c31a4e01/lua/mini/surround.lua#LL1048C13-L1048C72
 				output = function()
@@ -24,40 +104,7 @@ return {
 					if not emmet then
 						return nil
 					end
-					local tag, residue
-					tag, residue = emmet:match("^%s*([^#.[]+)(.+)")
-					local attr = {
-						["#"] = {},
-						["."] = {},
-						["[]"] = {},
-					}
-					for a in residue:gmatch("%b[]") do
-						local value = string.match(a, ".(.*).")
-						table.insert(attr["[]"], value)
-					end
-					for a in residue:gsub("%b[]", ""):gmatch("[.#][^.#]+") do
-						local key, value = string.match(a, "(.)(.+)")
-						table.insert(attr[key], value)
-					end
-
-					if #attr["#"] > 1 then
-						error("id should be unique")
-					end
-
-					local left = { tag }
-					if attr["#"][1] then
-						table.insert(left, string.format('id="%s"', attr["#"][1]))
-					end
-					if attr["."][1] then
-						table.insert(left, string.format('class="%s"', table.concat(attr["."], " ")))
-					end
-					if attr["[]"][1] then
-						table.insert(left, attr["[]"][1] and table.concat(attr["[]"], " "))
-					end
-					return {
-						left = string.format("<%s>", table.concat(left, " ")),
-						right = string.format("</%s>", tag),
-					}
+					return totag(emmet)
 				end,
 			},
 		},
