1ed4ed3d709d6e52f988bbf7772b108f1fb9f088
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-10-30 19:26:26 +0000

    nvim-file-location:fix keymap

--- nvim/lua/plugin/nvim-file-location.lua
+++ nvim/lua/plugin/nvim-file-location.lua
@@ -1,8 +1,17 @@
 return {
 	"diegoulloao/nvim-file-location",
-	keys = { "<leader>L" },
+	keys = {
+		{
+			"<leader>L",
+			function()
+				require("nvim-file-location").copy_file_location("absolute", true, false)
+			end,
+			mode = { "n" },
+			desc = "Copy absolute path to clipboard",
+		},
+	},
 	opts = {
-		keymap = "<leader>L",
+		keymap = nil,
 		mode = "absolute", -- options: workdir | absolute
 		add_line = true,
 		add_column = false,
