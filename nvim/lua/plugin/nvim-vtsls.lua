3ae57c32291530feb14d19469aa91708b0545bff
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-11-03 09:18:05 +0000

    vtsls: fix keymap

--- nvim/lua/plugin/nvim-vtsls.lua
+++ nvim/lua/plugin/nvim-vtsls.lua
@@ -44,7 +44,7 @@ return {
 						commands.remove_unused_imports()
 						-- commands.organize_imports()
 					end, { desc = "Organize imports", buffer = buffer })
-					vim.keymap.set("gD", "<leader>to", function()
+					vim.keymap.set("n", "gD", function()
 						commands.goto_source_definition()
 					end, { desc = "Go to source definition", buffer = buffer })
 
