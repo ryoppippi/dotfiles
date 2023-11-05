31123ecf545c2f87c42e5f09fcbf0cd4ecf0b02e
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-11-05 15:33:28 +0000

    nvim-lspconfig: denols config fix

--- nvim/lua/plugin/nvim-lspconfig/init.lua
+++ nvim/lua/plugin/nvim-lspconfig/init.lua
@@ -241,16 +241,18 @@ return {
 					end,
 				})
 			end,
-			init_options = {
-				lint = true,
-				unstable = true,
-				suggest = {
-					imports = {
-						hosts = {
-							["https://deno.land"] = true,
-							["https://cdn.nest.land"] = true,
-							["https://crux.land"] = true,
-							["https://esm.sh"] = true,
+			settings = {
+				deno = {
+					lint = true,
+					unstable = true,
+					suggest = {
+						imports = {
+							hosts = {
+								["https://deno.land"] = true,
+								["https://cdn.nest.land"] = true,
+								["https://crux.land"] = true,
+								["https://esm.sh"] = true,
+							},
 						},
 					},
 				},
