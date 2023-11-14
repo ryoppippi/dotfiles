1726f9bbaaf15b6af4c07d4e6b8094ba5cda525d
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-11-14 21:44:31 +0000

    python_tools: fix build config

--- nvim/lua/cli/python_tools/init.lua
+++ nvim/lua/cli/python_tools/init.lua
@@ -2,9 +2,7 @@ return {
 	name = "python_tools",
 	dir = vim.fn.fnamemodify(debug.getinfo(1).source:sub(2), ":h"),
 	dependencies = { "nvim-lua/plenary.nvim" },
-	build = function(spec)
-		vim.system({ "rye", "sync" }, { cwd = spec.dir, text = true })
-	end,
+	build = "rye sync",
 	config = function(spec)
 		local Path = require("plenary.path")
 
