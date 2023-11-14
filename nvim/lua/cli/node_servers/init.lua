e6a2d6ea11197dcbdd4cd5a0bb7626ae59a09056
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-11-14 15:42:02 +0000

    fix node_server config

--- nvim/lua/cli/node_servers/init.lua
+++ nvim/lua/cli/node_servers/init.lua
@@ -1,18 +1,17 @@
-local CONFIG_DIR = vim.fn.stdpath("config")
-local NODE_SERVERS_DIR = CONFIG_DIR .. "/lua/cli/node_servers"
-
 return {
 	name = "node_servers",
-	dir = "",
-	build = function()
-		vim.system({ "bun", "install" }, { cwd = NODE_SERVERS_DIR, text = true })
-	end,
+	dir = vim.fn.stdpath("config") .. "/lua/cli/node_servers",
+	build = "bun install",
+	init = function(spec)
+		local BIN_DIR = spec.dir .. "/node_modules/.bin"
 
-	init = function()
-		-- add ./node_modules/.bin to $PATH
-		local BIN_DIR = NODE_SERVERS_DIR .. "/node_modules/.bin"
-		vim.env.PATH = BIN_DIR .. ":" .. vim.env.PATH
+		if tb(vim.fn.isdirectory(BIN_DIR)) then
+			vim.env.PATH = BIN_DIR .. ":" .. vim.env.PATH
+		end
 
-		vim.system({ "bun", "i" }, { cwd = BIN_DIR, text = true })
+		vim.defer_fn(function()
+			vim.system({ "bun", "i" }, { cwd = spec.dir, text = true })
+			vim.env.PATH = BIN_DIR .. ":" .. vim.env.PATH
+		end, 0)
 	end,
 }
