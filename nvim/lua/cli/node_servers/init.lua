a8b1bd35d397e94fc8f353308a3cf86f4d7bb271
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-11-14 21:34:03 +0000

    nvim: use plenary.Path for safe filepath management

--- nvim/lua/cli/node_servers/init.lua
+++ nvim/lua/cli/node_servers/init.lua
@@ -1,15 +1,17 @@
 return {
 	name = "node_servers",
 	dir = vim.fn.fnamemodify(debug.getinfo(1).source:sub(2), ":h"),
+	dependencies = { "nvim-lua/plenary.nvim" },
 	build = "bun install",
 	config = function(spec)
+		local Path = require("plenary.path")
 		local dir = spec.dir
-		local BIN_DIR = dir .. "/node_modules/.bin"
-		if not tb(vim.fn.isdirectory(BIN_DIR)) then
-			vim.fn.mkdir(BIN_DIR, "p")
+		local BIN_DIR = Path:new(dir, "node_modules", ".bin")
+		if not tb(BIN_DIR:exists()) then
+			BIN_DIR:mkdir({ parents = true })
 		end
 
-		vim.env.PATH = BIN_DIR .. ":" .. vim.env.PATH
+		vim.env.PATH = BIN_DIR:absolute() .. ":" .. vim.env.PATH
 		vim.system({ "bun", "i" }, { cwd = dir, text = true })
 	end,
 }
