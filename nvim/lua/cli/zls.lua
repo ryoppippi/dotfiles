81d41f78641db26cfccaf6a2fba281137505e93b
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2023-11-14 16:01:28 +0000

    fix node_server and zls config

--- nvim/lua/cli/zls.lua
+++ nvim/lua/cli/zls.lua
@@ -3,4 +3,8 @@ return {
 	build = {
 		"bun x @oven/zig build -Doptimize=ReleaseSafe --verbose",
 	},
+	config = function(spec)
+		local dir = spec.dir
+		vim.system({ "bun", "i" }, { cwd = dir .. "/zig-out/bin", text = true })
+	end,
 }
