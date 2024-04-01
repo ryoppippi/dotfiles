b2fb143889e9980fbb1957c3df20dd735bff35ac
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-04-01 08:35:31 +0100

    no-neck-pain.nvim: add

---
+++ nvim/lua/plugin/no-neck-pain.lua
@@ -0,0 +1,12 @@
+return {
+	"shortcuts/no-neck-pain.nvim",
+	version = "*",
+	event = "UiEnter",
+	width = 120,
+	opts = {
+		autocmds = {
+			enableOnVimEnter = true,
+		},
+	},
+	config = true,
+}
