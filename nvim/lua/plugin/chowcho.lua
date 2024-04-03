cd8326aafd4d93687b90a483f7e17c39dcb760e3
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-04-03 21:41:43 +0100

    chowcho.nvim: update config(set labels and fix keymaps)

--- nvim/lua/plugin/chowcho.lua
+++ nvim/lua/plugin/chowcho.lua
@@ -1,6 +1,11 @@
 return {
 	"tkmpypy/chowcho.nvim",
 	keys = {
-		{ "<Leader>wq", "<CMD>Chowcho<CR>" },
+		{ "<Leader><cr>", "<CMD>Chowcho<CR>" },
+	},
+	opts = {
+		-- stylua: ignore
+		labels = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"},
+		ignore_case = true,
 	},
 }
