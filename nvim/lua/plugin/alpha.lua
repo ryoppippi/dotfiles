c46ac034887a93e1806ec3c98df349af29d535b2
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-04-03 09:07:26 +0100

    alpha.nvim: startify -> theta

--- nvim/lua/plugin/alpha.lua
+++ nvim/lua/plugin/alpha.lua
@@ -2,10 +2,10 @@ return {
 	"goolord/alpha-nvim",
 	lazy = false,
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		local alpha = require("alpha")
-		local startify = require("alpha.themes.startify")
-		startify.section.header.val = {
+	opts = function()
+		local theta = require("alpha.themes.theta")
+		local dashboard = require("alpha.themes.dashboard")
+		theta.header.val = {
 			[[                                   __                ]],
 			[[      ___     ___    ___   __  __ /\_\    ___ ___    ]],
 			[[     / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
@@ -13,15 +13,18 @@ return {
 			[[    \ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
 			[[     \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
 		}
-		startify.section.top_buttons.val = {
-			startify.button("e", "  New file", "<cmd>enew<cr>"),
-			startify.button("s", "  Restore Session", '<cmd>lua require("persistence").load()<cr>'),
-			startify.button("f", "  Files", "<cmd>Telescope find_files<cr>"),
-			startify.button(".", "󰈙  Oil", "<cmd>Oil<cr>"),
-			startify.button("r", "  MRU", "<cmd>Telescope oldfiles<cr>"),
-			startify.button("d", "  dotfiles", "<cmd>Config<cr>"),
-			startify.button("z", "󰒲  Lazy", "<cmd>Lazy<cr>"),
+		theta.buttons.val = {
+			dashboard.button("e", "  New file", "<cmd>enew<cr>"),
+			dashboard.button("s", "  Restore Session", '<cmd>lua require("persistence").load()<cr>'),
+			dashboard.button("f", "  Files", "<cmd>Telescope find_files<cr>"),
+			dashboard.button(".", "󰈙  Oil", "<cmd>Oil<cr>"),
+			dashboard.button("r", "  MRU", "<cmd>Telescope oldfiles<cr>"),
+			dashboard.button("d", "  dotfiles", "<cmd>Config<cr>"),
+			dashboard.button("z", "󰒲  Lazy", "<cmd>Lazy<cr>"),
 		}
-		alpha.setup(startify.config)
+		return theta.config
+	end,
+	config = function(_, opts)
+		require("alpha").setup(opts)
 	end,
 }
