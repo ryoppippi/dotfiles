b8d58cb46212639615aea6e1752554980e63e493
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-04-08 10:38:09 +0100

    fish&git: delete git command alias

--- fish/user_functions/fbr.fish
+++
@@ -1,7 +0,0 @@
-function fbr -d "Fuzzy-find and checkout a branch"
-    git branch --all | grep -v HEAD | string trim | fzf --inline-info | read -l result; and git checkout "$result"
-end
-
-function fcoc -d "Fuzzy-find and checkout a commit"
-    git log --pretty=oneline --abbrev-commit --reverse | fzf --tac +s -e | awk '{print $1;}' | read -l result; and git checkout "$result"
-end
