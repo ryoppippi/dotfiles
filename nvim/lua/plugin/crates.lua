9bde4801af6724c4d81fc5e8def3abf79297f8e5
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-04-08 23:16:44 +0100

    crates.nvim: fix config(deps and lsp config)

--- nvim/lua/plugin/crates.lua
+++ nvim/lua/plugin/crates.lua
@@ -1,8 +1,28 @@
 return {
 	"saecki/crates.nvim",
 	event = { "BufRead Cargo.toml" },
-	requires = { { "nvim-lua/plenary.nvim" } },
-	config = function()
-		require("crates").setup()
+	requires = {
+		"nvim-lua/plenary.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	init = function()
+		require("core.plugin").on_attach(function(client, bufnr)
+			local opts = function(desc)
+				return { silent = true, buffer = bufnr, desc = desc }
+			end
+			if client.name == "crates.nvim" then
+				local crates = require("crates")
+				vim.keymap.set("n", "gd", crates.show_dependencies_popup, opts("Show Crate dependencies popup"))
+				vim.keymap.set("n", "gl", crates.show_features_popup, opts("Show Crate features popup"))
+			end
+		end)
 	end,
+	opts = {
+		lsp = {
+			enabled = true,
+			actions = true,
+			completion = true,
+			hover = true,
+		},
+	},
 }
