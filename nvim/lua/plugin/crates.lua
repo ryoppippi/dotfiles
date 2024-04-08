7e5904334d583108a3e8d1c5186e9426ccc9c563
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-04-08 23:28:08 +0100

    crates.nvim: add keybindings for open repos/documents

--- nvim/lua/plugin/crates.lua
+++ nvim/lua/plugin/crates.lua
@@ -14,6 +14,9 @@ return {
 				local crates = require("crates")
 				vim.keymap.set("n", "gd", crates.show_dependencies_popup, opts("Show Crate dependencies popup"))
 				vim.keymap.set("n", "gl", crates.show_features_popup, opts("Show Crate features popup"))
+				vim.keymap.set("n", "gk", crates.open_documentation, opts("Open Crate documentation"))
+				vim.keymap.set("n", "gK", crates.open_crates_io, opts("Open Crate on crates.io"))
+				vim.keymap.set("n", "gL", crates.open_repository, opts("Open Crate repository"))
 			end
 		end)
 	end,
