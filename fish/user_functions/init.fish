d571163545b5291e4eddd044bfd396fdedef9bce
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-04-08 09:01:12 +0100

    fish: delete gh auth setup-git

--- fish/user_functions/init.fish
+++ fish/user_functions/init.fish
@@ -1,5 +1,4 @@
 function init -d init
-    type -q gh && gh auth setup-git
     type -q deno && deno --version >>/dev/null
     type -q bun && bun --version >>/dev/null
     type -q volta && volta install node @antfu/ni
