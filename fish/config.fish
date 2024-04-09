5c2073854bf98d05c2e909047f9e9ac1bbe27bca
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-04-09 20:42:30 +0100

    fish: separate brew config to other file

--- fish/config.fish
+++ fish/config.fish
@@ -27,14 +27,6 @@ fish_add_path $HOME/.local/bin
 fish_add_path /usr/local/opt/coreutils/libexec/gnubin
 fish_add_path /usr/local/opt/curl/bin
 
-# brew
-set -gx HOMEBREW_BUNDLE_FILE $HOME/.Brewfile
-set -gx HOMEBREW_NO_AUTO_UPDATE 1
-set -gx HOMEBREW_ARM /opt/homebrew
-set -gx HOMEBREW_X86_64 /usr/local
-fish_add_path $HOMEBREW_ARM/bin
-fish_add_path $HOMEBREW_X86_64/bin
-
 # aqua
 set -gx AQUA_ROOT_DIR $XDG_DATA_HOME/aquaproj-aqua
 set -gx AQUA_GLOBAL_CONFIG $XDG_CONFIG_HOME/aquaproj-aqua/aqua.yaml
