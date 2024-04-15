fd0955c4884cf2a232f4afc0dc139ac9629907c5
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-04-15 14:01:44 +0100

    wezterm: separate tab bar config

--- wezterm/colors/kanagawa_dragon.lua
+++ wezterm/colors/kanagawa_dragon.lua
@@ -1,7 +1,4 @@
 return {
-	force_reverse_video_cursor = true,
-	use_fancy_tab_bar = false,
-	window_background_opacity = 0.9,
 	colors = {
 		foreground = "#c5c9c5",
 		background = "#181616",
@@ -19,48 +16,5 @@ return {
 		ansi = { "#0D0C0C", "#C4746E", "#8A9A7B", "#C4B28A", "#8BA4B0", "#A292A3", "#8EA4A2", "#C8C093" },
 		brights = { "#A6A69C", "#E46876", "#87A987", "#E6C384", "#7FB4CA", "#938AA9", "#7AA89F", "#C5C9C5" },
 		indexed = { [16] = "#B6927B", [17] = "#B98D7B" },
-
-		tab_bar = {
-			background = "#1b1f2f",
-
-			active_tab = {
-				bg_color = "#444b71",
-				fg_color = "#c6c8d1",
-				intensity = "Normal",
-				underline = "None",
-				italic = false,
-				strikethrough = false,
-			},
-
-			inactive_tab = {
-				bg_color = "#282d3e",
-				fg_color = "#c6c8d1",
-				intensity = "Normal",
-				underline = "None",
-				italic = false,
-				strikethrough = false,
-			},
-
-			inactive_tab_hover = {
-				bg_color = "#1b1f2f",
-				fg_color = "#c6c8d1",
-				intensity = "Normal",
-				underline = "None",
-				italic = true,
-				strikethrough = false,
-			},
-
-			new_tab = {
-				bg_color = "#1b1f2f",
-				fg_color = "#c6c8d1",
-				italic = false,
-			},
-
-			new_tab_hover = {
-				bg_color = "#444b71",
-				fg_color = "#c6c8d1",
-				italic = false,
-			},
-		},
 	},
 }
--- wezterm/colors/onedark.lua
+++ wezterm/colors/onedark.lua
@@ -1,7 +1,4 @@
 return {
-	force_reverse_video_cursor = true,
-	use_fancy_tab_bar = false,
-	window_background_opacity = 0.96,
 	colors = {
 		foreground = "#e6efff",
 		background = "#1e2127",
@@ -20,43 +17,5 @@ return {
 
 		ansi = { "#1e2127", "#e06c75", "#98c379", "#d19a66", "#61afef", "#c678dd", "#56b6c2", "#828791" },
 		brights = { "#5c6370", "#e06c75", "#98c379", "#d19a66", "#61afef", "#c678dd", "#56b6c2", "#e6efff" },
-
-		tab_bar = {
-			background = "#1b1f2f",
-			active_tab = {
-				bg_color = "#444b71",
-				fg_color = "#c6c8d1",
-				intensity = "Normal",
-				underline = "None",
-				italic = false,
-				strikethrough = false,
-			},
-			inactive_tab = {
-				bg_color = "#282d3e",
-				fg_color = "#c6c8d1",
-				intensity = "Normal",
-				underline = "None",
-				italic = false,
-				strikethrough = false,
-			},
-			inactive_tab_hover = {
-				bg_color = "#1b1f2f",
-				fg_color = "#c6c8d1",
-				intensity = "Normal",
-				underline = "None",
-				italic = true,
-				strikethrough = false,
-			},
-			new_tab = {
-				bg_color = "#1b1f2f",
-				fg_color = "#c6c8d1",
-				italic = false,
-			},
-			new_tab_hover = {
-				bg_color = "#444b71",
-				fg_color = "#c6c8d1",
-				italic = false,
-			},
-		},
 	},
 }
