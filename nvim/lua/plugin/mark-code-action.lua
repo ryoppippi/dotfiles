55408e503c58b0472b93e0c67d08f9ac3fd56e1d
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-04-20 19:08:22 +0100

    mark-code-action.nvim: add

---
+++ nvim/lua/plugin/mark-code-action.lua
@@ -0,0 +1,10 @@
+---@type LazySpec
+return {
+	"https://github.com/crwebb85/mark-code-action.nvim",
+	config = true,
+	cmd = {
+		"MarkCodeActionMark",
+		"MarkCodeActionRun",
+		"MarkCodeActionInspect",
+	},
+}
