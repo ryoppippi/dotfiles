1bf7bcee15b5dc59e95a75d114d4ee66c0f32694
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-04-21 12:48:37 +0100

    nvim: add Quicklook command

--- nvim/lua/config/command.lua
+++ nvim/lua/config/command.lua
@@ -39,3 +39,10 @@ vim.api.nvim_create_user_command("ToggleCursorline", function()
 	vim.wo.cursorline = not vim.wo.cursorline
 	vim.wo.cursorcolumn = not vim.wo.cursorcolumn
 end, { nargs = 0, force = true })
+
+vim.api.nvim_create_user_command("QuickLook", function()
+	-- get current buffer absolute path
+	local path = vim.fn.expand("%:p")
+
+	require("core.utils").open_file_with_quicklook(path)
+end, { nargs = 0, force = true })
