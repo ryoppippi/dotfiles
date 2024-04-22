55e47e8f33e89b479717a2447b1a221b4937ac67
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-04-22 19:11:03 +0100

    ryoppippi-macro-colorscheme.nvim: create

---
+++ nvim/lua/plugin/ryoppippi-macro-colorscheme.lua
@@ -0,0 +1,79 @@
+local getColorScheme = function()
+	local cs = vim.g.colors_name
+	if cs == "kanagawa" then
+		local ok, kanagawa = pcall(require, "kanagawa")
+		if not ok then
+			vim.notify("kanagawa is not installed", vim.log.levels.ERROR)
+			return cs
+		end
+
+		return ("kanagawa-%s"):format(kanagawa._CURRENT_THEME)
+	end
+	return cs
+end
+
+---@type LazySpec
+return {
+	dir = "",
+	name = "ryoppippi-macro-colorscheme",
+	event = "BufEnter",
+	opts = {
+		macro_cs = "kanagawa-wave",
+		check_interval = 200,
+	},
+	config = function(_, opts)
+		local debounce = require("core.utils").debounce
+		local prev_cs = nil
+
+		local Popup = require("nui.popup")
+		local popup = Popup({
+			enter = false,
+			focusable = false,
+			zindex = 100,
+			border = {
+				style = "single",
+			},
+			relative = "cursor",
+			size = {
+				width = 1,
+				height = 1,
+			},
+			position = {
+				row = -3,
+				col = -1,
+			},
+		})
+
+		vim.api.nvim_create_autocmd("CursorMoved", {
+			pattern = "*",
+			callback = function()
+				popup:update_layout()
+			end,
+		})
+
+		local changeColorScheme = debounce(
+			vim.schedule_wrap(function()
+				local reg = vim.fn.reg_recording()
+				-- Show popup
+				if reg ~= "" then
+					popup:mount()
+					vim.api.nvim_buf_set_lines(popup.bufnr, 0, -1, false, { reg })
+					local crrent_cs = getColorScheme()
+					if crrent_cs ~= opts.macro_cs then
+						prev_cs = crrent_cs
+						vim.cmd.colorscheme(opts.macro_cs)
+					end
+				else
+					if prev_cs ~= nil then
+						popup:unmount()
+						vim.cmd.colorscheme(prev_cs)
+						prev_cs = nil
+					end
+				end
+			end),
+			opts.check_interval
+		)
+
+		vim.on_key(changeColorScheme)
+	end,
+}
