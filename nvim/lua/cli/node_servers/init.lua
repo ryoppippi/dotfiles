5ebc8c6d5161d1c1ee4d8ff1a1e1fc91b5b9cf2f
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-05-03 13:39:47 +0100

    feat(node_servers): simplify bin directory creation and update
    This commit simplifies the process of creating the bin directory
    and updating it. It removes the dependency on 'plenary.nvim' and
    instead uses 'bun' for installation. The bin directory is now
    asserted directly from the output of 'bun pm bin', making the
    process more efficient and less error-prone.

--- nvim/lua/cli/node_servers/init.lua
+++ nvim/lua/cli/node_servers/init.lua
@@ -1,17 +1,14 @@
 return {
 	name = "node_servers",
 	dir = vim.fn.fnamemodify(debug.getinfo(1).source:sub(2), ":h"),
-	dependencies = { "nvim-lua/plenary.nvim" },
 	build = "bun install",
 	config = function(spec)
-		local Path = require("plenary.path")
 		local dir = spec.dir
-		local BIN_DIR = Path:new(dir, "node_modules", ".bin")
-		if not tb(BIN_DIR:exists()) then
-			BIN_DIR:mkdir({ parents = true })
-		end
 
-		vim.env.PATH = BIN_DIR:absolute() .. ":" .. vim.env.PATH
-		vim.system({ "bun", "i" }, { cwd = dir, text = true }):wait()
+		vim.system({ "bun", "i" }, { cwd = dir, text = true }, _l("obj: vim.print(obj.stdout)"))
+
+		local BIN_DIR =
+			assert((vim.system({ "bun", "pm", "bin" }, { cwd = dir, text = true }):wait()).stdout):gsub("\n[^\n]*$", "")
+		vim.env.PATH = ("%s:%s"):format(BIN_DIR, vim.env.PATH)
 	end,
 }
