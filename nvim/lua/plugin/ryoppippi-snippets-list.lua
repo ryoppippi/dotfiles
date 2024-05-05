9ba891151f659cc13b9533d345982091bc582b6f
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-05-05 15:10:07 +0100

    feat(snippets): Consolidate snippet lists into a single file
    This commit consolidates the various snippet lists into a single
    file named 'ryoppippi-snippets-list.lua'. This change makes it easier
    to manage and update the list of snippets used in the project.

---
+++ nvim/lua/plugin/ryoppippi-snippets-list.lua
@@ -0,0 +1,20 @@
+---@type LazySpec
+return {
+	dir = "",
+	name = "ryoppippi-snippets-list",
+	dependencies = {
+		"craigmac/vim-vsnip-snippets",
+		"honza/vim-snippets",
+		"rafamadriz/friendly-snippets",
+
+		-- language specific snippets
+		-- python
+		"cstrap/flask-snippets",
+		"cstrap/python-snippets",
+		-- zig
+		"Metalymph/zig-snippets",
+		-- web
+		"fivethree-team/vscode-svelte-snippets",
+		"xabikos/vscode-javascript",
+	},
+}
