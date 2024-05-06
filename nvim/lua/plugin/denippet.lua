a70c712b9e4c673196675068569d4d6a7deb62ba
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-05-06 16:49:00 +0100

    feat(denippet): add config for choosing

--- nvim/lua/plugin/denippet.lua
+++ nvim/lua/plugin/denippet.lua
@@ -33,8 +33,91 @@ return {
 			"vim-denops/denops.vim",
 			"ryoppippi-snippets-list",
 		},
+		init = function()
+			vim.cmd([[
+				let g:denops#debug = 1 
+			]])
+		end,
 		config = function()
 			require("denops-lazy").load("denippet-autoimport-vscode")
+
+			local Popup = {}
+
+			function Popup.new()
+				return setmetatable({
+					nsid = vim.api.nvim_create_namespace("denippet_choice_popup"),
+				}, { __index = Popup })
+			end
+
+			function Popup:open(node)
+				self.buf = vim.api.nvim_create_buf(false, true)
+				vim.api.nvim_buf_set_text(self.buf, 0, 0, 0, 0, node.items)
+				local w, h = vim.lsp.util._make_floating_popup_size(node.items, {})
+				self.win = vim.api.nvim_open_win(self.buf, false, {
+					relative = "win",
+					width = w,
+					height = h,
+					bufpos = { node.range.start.line, node.range.start.character },
+					style = "minimal",
+					border = "single",
+				})
+				self.extmark = vim.api.nvim_buf_set_extmark(
+					self.buf,
+					self.nsid,
+					node.index,
+					0,
+					{ hl_group = "incsearch", end_line = node.index + 1 }
+				)
+			end
+
+			function Popup:update()
+				vim.api.nvim_buf_del_extmark(self.buf, self.nsid, self.extmark)
+				local node = vim.b.denippet_current_node
+				self.extmark = vim.api.nvim_buf_set_extmark(
+					self.buf,
+					self.nsid,
+					node.index,
+					0,
+					{ hl_group = "incsearch", end_line = node.index + 1 }
+				)
+			end
+
+			function Popup:close()
+				if self.win then
+					vim.api.nvim_win_close(self.win, true)
+					self.buf = nil
+					self.win = nil
+					self.extmark = nil
+				end
+			end
+
+			local popup = Popup.new()
+
+			local group = vim.api.nvim_create_augroup("denippet_choice_popup", {})
+			vim.api.nvim_create_autocmd("User", {
+				group = group,
+				pattern = "DenippetNodeEnter",
+				callback = function()
+					local node = vim.b.denippet_current_node
+					if node and node.type == "choice" then
+						popup:open(node)
+					end
+				end,
+			})
+			vim.api.nvim_create_autocmd("User", {
+				group = group,
+				pattern = "DenippetChoiceSelected",
+				callback = function()
+					popup:update()
+				end,
+			})
+			vim.api.nvim_create_autocmd("User", {
+				group = group,
+				pattern = "DenippetNodeLeave",
+				callback = function()
+					popup:close()
+				end,
+			})
 		end,
 	},
 }
