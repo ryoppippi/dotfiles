7142b188ab68ddf6a2a252b56a5cadbf0aafe465
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-05-08 07:40:56 +0100

    feat(core/plugin.lua/on_attach): assert client existence in LSP attach
    The change ensures that the client exists when the Language Server
    Protocol (LSP) is attached. This is done by using the 'assert' function
    on the client retrieved by its ID. This will throw an error if the
    client does not exist, making it easier to debug issues related to LSP
    attachments.

--- nvim/lua/core/plugin.lua
+++ nvim/lua/core/plugin.lua
@@ -22,7 +22,7 @@ function M.on_attach(on_attach)
 	vim.api.nvim_create_autocmd("LspAttach", {
 		callback = function(args)
 			local buffer = args.buf
-			local client = vim.lsp.get_client_by_id(args.data.client_id)
+			local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
 			on_attach(client, buffer)
 		end,
 	})
