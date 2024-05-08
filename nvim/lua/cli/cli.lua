3a393406800815985e497b1d1afd7f4551ab1f6e
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-05-08 11:42:00 +0100

    feat: zig and zls are installed via Aqua
    This commit adds the Aqua package manager to the project.
    Also we uninstall zig and zls from lazy.nvim and mise.
    
    BREAKING CHANGE: Zig and ZLS are now managed by Aqua. Ensure Aqua is
    properly set up and updated to use the new versions.

--- nvim/lua/cli/cli.lua
+++ nvim/lua/cli/cli.lua
@@ -8,26 +8,27 @@ return {
 		{ "Shopify/ruby-lsp", version = "*", build = { "gem install --user-install --no-document --pre ruby-lsp" } },
 
 		--- zig
-		{
-			"zigtools/zls",
-			tag = "0.12.0",
-			build = {
-				"zig build -Doptimize=ReleaseSafe --verbose",
-			},
-			config = function(spec)
-				local Path = require("plenary.path")
-
-				local dir = spec.dir
-
-				local BIN_DIR = Path:new(dir, "zig-out", "bin")
-
-				if not (BIN_DIR:exists()) then
-					BIN_DIR:mkdir({ parents = true })
-				end
-
-				vim.env.PATH = BIN_DIR:absolute() .. ":" .. vim.env.PATH
-			end,
-		},
+		--- currently installed in aqua
+		-- {
+		-- 	"zigtools/zls",
+		-- 	tag = "0.12.0",
+		-- 	build = {
+		-- 		"zig build -Doptimize=ReleaseSafe --verbose",
+		-- 	},
+		-- 	config = function(spec)
+		-- 		local Path = require("plenary.path")
+		--
+		-- 		local dir = spec.dir
+		--
+		-- 		local BIN_DIR = Path:new(dir, "zig-out", "bin")
+		--
+		-- 		if not (BIN_DIR:exists()) then
+		-- 			BIN_DIR:mkdir({ parents = true })
+		-- 		end
+		--
+		-- 		vim.env.PATH = BIN_DIR:absolute() .. ":" .. vim.env.PATH
+		-- 	end,
+		-- },
 
 		--- rust
 		{
