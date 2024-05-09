33080bf39253b3857da02a1189c8839f7a5979ee
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-05-09 12:50:56 +0100

    feat(lambdalisue): みんな大好きありすえラーメン :ramen:

--- nvim/lua/plugin/gin.lua
+++ nvim/lua/plugin/gin.lua
@@ -1,8 +1,8 @@
 return {
-	"lambdalisue/gin.vim",
+	"lambdalisue/vim-gin",
 	event = { "User DenopsReady" },
 	dependencies = { "vim-denops/denops.vim" },
 	config = function()
-		require("denops-lazy").load("gin.vim")
+		require("denops-lazy").load("vim-gin")
 	end,
 }
