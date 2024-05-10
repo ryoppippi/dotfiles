ebd8b7306c415a73071969b3b7f180ebb27d00aa
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-05-10 13:35:59 +0100

    refactor(cli): remove unused dependencies
    This commit removes the unused dependencies from the cli.lua file. The
    dependencies for ruby-lsp, zls, and rust-analyzer have been removed as
    they are no longer needed. This change simplifies the configuration and
    reduces the potential for issues related to these dependencies.

--- nvim/lua/cli/cli.lua
+++ nvim/lua/cli/cli.lua
@@ -4,40 +4,6 @@ return {
 	dir = "",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		--- ruby
-		{ "Shopify/ruby-lsp", version = "*", build = { "gem install --user-install --no-document --pre ruby-lsp" } },
-
-		--- zig
-		--- currently installed in aqua
-		-- {
-		-- 	"zigtools/zls",
-		-- 	tag = "0.12.0",
-		-- 	build = {
-		-- 		"zig build -Doptimize=ReleaseSafe --verbose",
-		-- 	},
-		-- 	config = function(spec)
-		-- 		local Path = require("plenary.path")
-		--
-		-- 		local dir = spec.dir
-		--
-		-- 		local BIN_DIR = Path:new(dir, "zig-out", "bin")
-		--
-		-- 		if not (BIN_DIR:exists()) then
-		-- 			BIN_DIR:mkdir({ parents = true })
-		-- 		end
-		--
-		-- 		vim.env.PATH = BIN_DIR:absolute() .. ":" .. vim.env.PATH
-		-- 	end,
-		-- },
-
-		--- rust
-		{
-			name = "rust-analyzer",
-			dir = "",
-			config = function()
-				vim.system({ "rustup", "component", "add", "rust-analyzer" }, { text = true })
-			end,
-		},
 
 		--- python
 		{
