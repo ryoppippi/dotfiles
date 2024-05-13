70349a74842e948a8be1cb04c55f727ce694217b
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-05-13 17:47:18 +0100

    feat(nvim): Add base-lazy-denops-plugin template
    This commit introduces a new file, base-lazy-denops-plugin.lua, in the
    nvim/template/lua directory. This file serves as a template for creating
    lazy-loaded denops plugins. It's configured to load when the "User
    DenopsReady" event is fired and depends on "vim-denops/denops.vim".

---
+++ nvim/template/lua/base-lazy-denops-plugin.lua
@@ -0,0 +1,11 @@
+
+---@type LazySpec
+return {
+  "{{_cursor_}}",
+	event = { "User DenopsReady" },
+	dependencies = { "vim-denops/denops.vim" },
+	config = function(spec)
+		require("denops-lazy").load(spec.name)
+	end,
+}
+
