aefce45cb264a402934982dc89a601fc8799efbd
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-05-13 19:03:39 +0100

    feat(nvim): add nvim-automa plugin
    This commit adds the nvim-automa plugin to the Neovim configuration. The
    plugin is added to the lazy-lock.json file with its corresponding branch
    and commit hash. Additionally, a new Lua file is created for the plugin
    under the nvim/lua/plugin directory. The plugin is set to load on the
    "VeryLazy" event.

---
+++ nvim/lua/plugin/automa.lua
@@ -0,0 +1,15 @@
+---@type LazySpec
+return {
+	"https://github.com/hrsh7th/nvim-automa",
+	event = "VeryLazy",
+	opts = {
+		mapping = {
+			-- ["."] = {
+			-- 	queries = {
+			-- 		-- wide-range dot-repeat definition.
+			-- 		{ "!n(h,j,k,l)+" },
+			-- 	},
+			-- },
+		},
+	},
+}
