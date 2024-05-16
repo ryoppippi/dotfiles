7cb0da67a379241d19bbcf49f4aab3060df23310
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-05-16 08:41:07 +0100

    feat(fish_user_key_bindings): remove deleted key bindings by fzf
    The key bindings for `__fzf_find_and_execute` and `__fzf_cd_with_hidden`
    functions were removed from fzf.

--- fish/user_functions/fish_user_key_bindings.fish
+++ fish/user_functions/fish_user_key_bindings.fish
@@ -23,28 +23,20 @@ function fish_user_key_bindings
     if test "$FZF_LEGACY_KEYBINDINGS" -eq 1
         bind \ct __fzf_find_file
         bind \cr __fzf_reverse_isearch
-        bind \cx __fzf_find_and_execute
         bind \ec __fzf_cd
-        bind \eC __fzf_cd_with_hidden
         if bind -M insert >/dev/null ^/dev/null
             bind -M insert \ct __fzf_find_file
             bind -M insert \cr __fzf_reverse_isearch
-            bind -M insert \cx __fzf_find_and_execute
             bind -M insert \ec __fzf_cd
-            bind -M insert \eC __fzf_cd_with_hidden
         end
     else
         bind \cf __fzf_find_file
         bind \cr __fzf_reverse_isearch
-        bind \ex __fzf_find_and_execute
         bind \ed __fzf_cd
-        bind \eD __fzf_cd_with_hidden
         if bind -M insert >/dev/null ^/dev/null
             bind -M insert \cf __fzf_find_file
             bind -M insert \cr __fzf_reverse_isearch
-            bind -M insert \ex __fzf_find_and_execute
             bind -M insert \ed __fzf_cd
-            bind -M insert \eD __fzf_cd_with_hidden
         end
     end
     ### fzf ###
