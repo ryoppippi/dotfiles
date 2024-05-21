a7ebd2e95206e2226b8219583f68f6fd3d870edb
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-05-21 11:33:49 +0100

    feat(key_bindings): add hybrid key bindings for fish shell
    This commit introduces a new function `fish_hybrid_key_bindings` that
    provides Vi-style bindings that inherit emacs-style bindings in all
    modes. These bindings are not enabled in vim/neovim terminal. The
    `fish_key_bindings` global variable is set to use these hybrid bindings.

---
+++ fish/config/key_bindings.fish
@@ -0,0 +1,14 @@
+function fish_hybrid_key_bindings --description \
+    "Vi-style bindings that inherit emacs-style bindings in all modes & not enabled in vim/neovim terminal"
+    for mode in default insert visual
+        fish_default_key_bindings -M $mode
+    end
+    if set -q NVIM
+        or set -q VIM_TERMINAL
+        fish_default_key_bindings
+    else
+        fish_vi_key_bindings --no-erase
+    end
+end
+
+set -g fish_key_bindings fish_hybrid_key_bindings
