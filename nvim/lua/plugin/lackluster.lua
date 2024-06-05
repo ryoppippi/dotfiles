2000b4f52643bc73b10c1a012207c23c5938211c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-06-05 10:43:57 +0100

    feat: Update lackluster.nvim and set as default colorscheme
    This commit updates the commit hash for lackluster.nvim in lazy-lock.json
    indicating a version update. It also sets lackluster as the default
    colorscheme in pack.lua. Additionally, it expands the configuration for
    lackluster in lackluster.lua, adding a new autocommand for the ColorScheme
    event and configuring various aspects of the colorscheme.

--- nvim/lua/plugin/lackluster.lua
+++ nvim/lua/plugin/lackluster.lua
@@ -2,18 +2,41 @@ local function isLackluster()
 	return vim.env.NVIM_COLORSCHEME == "lackluster"
 end
 
+local COLORSCHEME_NAME = "lackluster"
+
 ---@type LazySpec
 return {
 	"slugbyte/lackluster.nvim",
 	priority = isLackluster() and 1000 or 50,
 	event = isLackluster() and { "UiEnter" } or { "VeryLazy" },
-	config = function()
-		if isLackluster() then
-			vim.cmd.colorscheme("lackluster")
-		end
+	init = function()
 		vim.api.nvim_create_autocmd("ColorScheme", {
-			pattern = "lackluster",
-			callback = function() end,
+			callback = function(args)
+				if not vim.startswith(args.match, "lackluster") then
+					return
+				end
+				vim.g.colors_name = args.match
+			end,
 		})
+	end,
+	config = function()
+		local lackluster = require("lackluster")
+		local color = lackluster.color -- blue, green, red, orange, black, lack, luster, gray1-9
+		lackluster.setup({
+			tweek_syntax = {
+				comment = lackluster.color.gray4, -- or gray5
+			},
+			tweek_background = {
+				normal = "none",
+				telescope = "none",
+				menu = lackluster.color.gray3,
+				popup = "default",
+			},
+		})
+
+		if isLackluster() then
+			vim.cmd.colorscheme(COLORSCHEME_NAME)
+			vim.g.colors_name = COLORSCHEME_NAME
+		end
 	end,
 }
