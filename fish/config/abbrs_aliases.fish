8cccc945d9d3f63a6adbc0392965539af824b267
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-06-19 14:37:10 +0100

    feat(fish/config): add deno task abbreviation
    A new abbreviation 'dt' for 'deno task' has been added to the
    fish shell configuration. This will allow for quicker execution
    of deno tasks.

--- fish/config/abbrs_aliases.fish
+++ fish/config/abbrs_aliases.fish
@@ -63,6 +63,7 @@ abbr -a ngc nix-collect-garbage
 # deno
 abbr -a dr "deno run -A --unstable"
 abbr -a deno-cache-clear "rm -rf (deno info | string match --entire --regex 'DENO_DIR*' | string split ' ')[-1]"
+abbr -a dt "deno task"
 
 abbr -a cpf "pbcopy < "
 abbr -a paf "pbpaste > "
