ac7039e89e6f50820a078d82bd630afefca9b222
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-07-07 12:11:28 +0100

    feat(karabiner): refactor keyboard device handling
    This commit refactors the handling of keyboard devices in the
    Karabiner configuration. The `ifNotSelfMadeKeyboard` constant is now
    exported from the `devices.ts` file, and the `toHideApp` function has
    been removed and replaced with an import from `utils.ts`. This change
    improves the modularity and readability of the code.

--- karabiner/devices.ts
+++ karabiner/devices.ts
@@ -1,6 +1,9 @@
-import type { DeviceIdentifier } from "karabiner_ts";
+import * as k from "karabiner_ts";
 
 export const CLAW44 = {
   product_id: 1,
   vendor_id: 22854,
-} as const satisfies DeviceIdentifier;
+} as const satisfies k.DeviceIdentifier;
+
+/** not apple keyboard */
+export const ifNotSelfMadeKeyboard = k.ifDevice([CLAW44]).unless();
