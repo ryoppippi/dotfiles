e062ae0459f51f62f446fe47b46fd7a9080ff55e
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-13 19:05:40 +0100

    feat(nvim): delete all plugin about quickfix

--- nvim/lua/plugin/nvim-pqf.lua
+++
@@ -1,6 +0,0 @@
----@type LazySpec
-return {
-	"https://github.com/yorickpeterse/nvim-pqf",
-	event = "VeryLazy",
-	config = true,
-}
--- nvim/lua/plugin/vim-qfreplace.lua
+++
@@ -1,6 +0,0 @@
----@type LazySpec
-return {
-	"thinca/vim-qfreplace",
-	ft = "qf",
-	cmd = { "Qfreplace" },
-}
