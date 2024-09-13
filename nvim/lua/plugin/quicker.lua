ed0ff75de35772368c838b23197856476f09135a
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-13 19:46:34 +0100

    fix(nvim): quicker.nvim option

--- nvim/lua/plugin/quicker.lua
+++ nvim/lua/plugin/quicker.lua
@@ -1,7 +1,29 @@
 ---@type LazySpec
 return {
 	"https://github.com/stevearc/quicker.nvim",
-	event = "FileType qf",
+	ft = { "qf" },
+	keys = {
+		{
+			"<leader>R",
+			function()
+				require("quicker").refresh()
+			end,
+		},
+	},
+	init = function()
+		vim.api.nvim_create_autocmd("BufReadPost", {
+			pattern = "qf",
+			once = true,
+			callback = function()
+				local refresh = require("quicker").refresh
+
+				refresh()
+				require("core.plugin").on_attach(function()
+					refresh()
+				end)
+			end,
+		})
+	end,
 	---@module "quicker"
 	---@type quicker.SetupOptions
 	opts = {},
