51ffdc2a96be656cdcfa6a139b46d3cab8eea99a
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-09-20 15:32:07 +0100

    feat(nvim): python -> python3 in Quickrun :manbou:

--- nvim/lua/plugin/vim-quickrun.lua
+++ nvim/lua/plugin/vim-quickrun.lua
@@ -2,6 +2,12 @@ return {
 	"thinca/vim-quickrun",
 	cmd = "QuickRun",
 	dependencies = {
+		{ "tani/vim-artemis" },
 		{ "lambdalisue/vim-quickrun-neovim-job" },
 	},
+	config = function()
+		vimx.g.quickrun_config = {
+			python = { command = "python3" },
+		}
+	end,
 }
