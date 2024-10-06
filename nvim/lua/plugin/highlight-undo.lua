170c6d7ddf40f30be1e411d54e0893de0541d69f
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-06 11:54:18 +0100

    feat(nvim): update hightlight-undo

--- nvim/lua/plugin/highlight-undo.lua
+++ nvim/lua/plugin/highlight-undo.lua
@@ -2,13 +2,5 @@ return {
 	{
 		"tzachar/highlight-undo.nvim",
 		event = "VeryLazy",
-		opts = {
-			hlgroup = "HighlightUndo",
-			duration = 300,
-			keymaps = {
-				{ "n", "u", "undo", {} },
-				{ "n", "<C-r>", "redo", {} },
-			},
-		},
 	},
 }
