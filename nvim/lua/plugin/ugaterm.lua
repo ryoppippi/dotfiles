56f02d4e75df3b7805e129075a3308c5664f8c3c
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-15 18:05:52 +0100

    feat(nvim): ugaterm floating

--- nvim/lua/plugin/ugaterm.lua
+++ nvim/lua/plugin/ugaterm.lua
@@ -4,4 +4,17 @@ return {
 	keys = {
 		{ "<c-z>", "<cmd>UgatermOpen -toggle<cr>", mode = { "n", "t" } },
 	},
+	opts = {
+		open_cmd = function()
+			local height = vim.api.nvim_get_option("lines")
+			local width = vim.api.nvim_get_option("columns")
+			vim.api.nvim_open_win(0, true, {
+				relative = "editor",
+				row = math.floor(height * 0.1),
+				col = math.floor(width * 0.1),
+				height = math.floor(height * 0.8),
+				width = math.floor(width * 0.8),
+			})
+		end,
+	},
 }
