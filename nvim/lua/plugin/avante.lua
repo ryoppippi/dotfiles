0a5aa72437ab8fc712ead7fae8e3b6d59deba230
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-22 22:34:58 +0100

    feat(nvim):img-clip

--- nvim/lua/plugin/avante.lua
+++ nvim/lua/plugin/avante.lua
@@ -18,23 +18,7 @@ return {
 		--- The below dependencies are optional,
 		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
 		"zbirenbaum/copilot.lua", -- for providers='copilot'
-		{
-			-- support for image pasting
-			"HakonHarnes/img-clip.nvim",
-			event = "VeryLazy",
-			opts = {
-				-- recommended settings
-				default = {
-					embed_image_as_base64 = false,
-					prompt_for_file_name = false,
-					drag_and_drop = {
-						insert_mode = true,
-					},
-					-- required for Windows users
-					use_absolute_path = true,
-				},
-			},
-		},
+		"HakonHarnes/img-clip.nvim",
 		{
 			-- Make sure to set this up properly if you have lazy=true
 			"MeanderingProgrammer/render-markdown.nvim",
