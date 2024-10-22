63859f399452eb865081b9cea21d3b0da4079e88
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-22 12:10:22 +0100

    refactor(wezterm): move tab config to tab_bar.lua

--- wezterm/tab_bar.lua
+++ wezterm/tab_bar.lua
@@ -1,45 +1,52 @@
-local tab_bar = {
-	background = "#1b1f2f",
-	inactive_tab_edge = "none",
+return {
+	tab_bar_at_bottom = false,
+	use_fancy_tab_bar = false,
+	hide_tab_bar_if_only_one_tab = true,
+	show_new_tab_button_in_tab_bar = false,
 
-	active_tab = {
-		bg_color = "#444b71",
-		fg_color = "#c6c8d1",
-		intensity = "Normal",
-		underline = "None",
-		italic = false,
-		strikethrough = false,
-	},
+	colors = {
+		tab_bar = {
+			background = "#1b1f2f",
+			inactive_tab_edge = "none",
 
-	inactive_tab = {
-		bg_color = "#282d3e",
-		fg_color = "#c6c8d1",
-		intensity = "Normal",
-		underline = "None",
-		italic = false,
-		strikethrough = false,
-	},
+			active_tab = {
+				bg_color = "#444b71",
+				fg_color = "#c6c8d1",
+				intensity = "Normal",
+				underline = "None",
+				italic = false,
+				strikethrough = false,
+			},
 
-	inactive_tab_hover = {
-		bg_color = "#1b1f2f",
-		fg_color = "#c6c8d1",
-		intensity = "Normal",
-		underline = "None",
-		italic = true,
-		strikethrough = false,
-	},
+			inactive_tab = {
+				bg_color = "#282d3e",
+				fg_color = "#c6c8d1",
+				intensity = "Normal",
+				underline = "None",
+				italic = false,
+				strikethrough = false,
+			},
 
-	new_tab = {
-		bg_color = "#1b1f2f",
-		fg_color = "#c6c8d1",
-		italic = false,
-	},
+			inactive_tab_hover = {
+				bg_color = "#1b1f2f",
+				fg_color = "#c6c8d1",
+				intensity = "Normal",
+				underline = "None",
+				italic = true,
+				strikethrough = false,
+			},
 
-	new_tab_hover = {
-		bg_color = "#444b71",
-		fg_color = "#c6c8d1",
-		italic = false,
+			new_tab = {
+				bg_color = "#1b1f2f",
+				fg_color = "#c6c8d1",
+				italic = false,
+			},
+
+			new_tab_hover = {
+				bg_color = "#444b71",
+				fg_color = "#c6c8d1",
+				italic = false,
+			},
+		},
 	},
 }
-
-return tab_bar
