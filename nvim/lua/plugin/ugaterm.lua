b9278c01c890c0cc5e49bd36a1366df1cda8cfd5
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-22 14:54:47 +0100

    feat(nvim): add opacity to floating window

--- nvim/lua/plugin/ugaterm.lua
+++ nvim/lua/plugin/ugaterm.lua
@@ -8,13 +8,16 @@ return {
 		open_cmd = function()
 			local height = vim.api.nvim_get_option("lines")
 			local width = vim.api.nvim_get_option("columns")
-			vim.api.nvim_open_win(0, true, {
+			local bufnr = vim.api.nvim_open_win(0, true, {
 				relative = "editor",
 				row = math.floor(height * 0.1),
 				col = math.floor(width * 0.1),
 				height = math.floor(height * 0.8),
 				width = math.floor(width * 0.8),
 			})
+
+			-- set winblend to 20
+			vim.api.nvim_win_set_option(bufnr, "winblend", 20)
 		end,
 	},
 }
