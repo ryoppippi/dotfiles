d3c0ee32710e26067aa90bb5a0c19717e42fcdfc
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-22 12:10:22 +0100

    refactor(wezterm): move tab config to tab_bar.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -42,12 +42,6 @@ local config = {
 		colors = { "#000000" },
 	},
 
-	colors = {
-		tab_bar = {
-			inactive_tab_edge = "none",
-		},
-	},
-
 	use_ime = true,
 	send_composed_key_when_left_alt_is_pressed = false,
 	send_composed_key_when_right_alt_is_pressed = false,
