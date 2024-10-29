7813e83f83e4ff06188ad738d99a274b6bce4965
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-10-29 19:41:48 +0000

    feat(nvim): add bun for quickrun

--- nvim/lua/plugin/vim-quickrun.lua
+++ nvim/lua/plugin/vim-quickrun.lua
@@ -1,6 +1,14 @@
+local function executable(cmd)
+	return tb(vim.fn.executable(cmd))
+end
+
+---@type LazySpec
 return {
 	"thinca/vim-quickrun",
 	cmd = "QuickRun",
+	keys = {
+		{ "qr", "QuickRun", mode = "ca" },
+	},
 	dependencies = {
 		{ "tani/vim-artemis" },
 		{ "lambdalisue/vim-quickrun-neovim-job" },
@@ -8,6 +16,16 @@ return {
 	config = function()
 		vimx.g.quickrun_config = {
 			python = { command = "python3" },
+			typescript = {
+				type = executable("deno") and "typescript/deno"
+					or executable("bun") and "typescript/bun"
+					or executable("ts-node") and "typescript/ts-node"
+					or "typescript/node",
+			},
+			["typescript/bun"] = {
+				command = "bun",
+				exec = "%c run %o %s",
+			},
 		}
 	end,
 }
