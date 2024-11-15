b406a7d678cfd46c7d447c49d6e1f345dff74fc8
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-11-15 19:17:08 +0000

    feat(nvim): update neotest (abbr & deno-test)

--- nvim/lua/plugin/neotest.lua
+++ nvim/lua/plugin/neotest.lua
@@ -2,6 +2,13 @@
 return {
 	"https://github.com/nvim-neotest/neotest",
 	cmd = { "Neotest" },
+	keys = {
+
+		{ "nt", "Neotest", mode = "ca" },
+		{ "ntr", "Neotest run", mode = "ca" },
+		{ "nts", "Neotest summary", mode = "ca" },
+		{ "nto", "Neotest output-panel", mode = "ca" },
+	},
 	dependencies = {
 		{
 			"nvim-neotest/nvim-nio",
@@ -10,11 +17,13 @@ return {
 			"nvim-treesitter/nvim-treesitter",
 		},
 		"https://github.com/marilari88/neotest-vitest",
+		"https://github.com/MarkEmmons/neotest-deno",
 	},
 	opts = function()
 		return {
 			adapters = {
 				require("neotest-vitest"),
+				require("neotest-deno"),
 			},
 		}
 	end,
