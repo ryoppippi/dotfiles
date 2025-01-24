a56cba5ef5a6ee2e4b52130f8553e3760f4736d7
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-01-24 18:22:51 +0000

    feat(plugin): add key mappings for Gin push and pull
    - Added key mappings for "Gin push" and "Gin pull" commands
    - Mappings: "gp" for push and "gpl" for pull in command mode

--- nvim/lua/plugin/gin.lua
+++ nvim/lua/plugin/gin.lua
@@ -3,6 +3,10 @@ return {
 	"lambdalisue/vim-gin",
 	event = { "User DenopsReady" },
 	dependencies = { "vim-denops/denops.vim" },
+	keys = {
+		{ "gp", "Gin push", mode = "ca" },
+		{ "gpl", "Gin pull", mode = "ca" },
+	},
 	config = function(spec)
 		require("denops-lazy").load(spec.name)
 	end,
