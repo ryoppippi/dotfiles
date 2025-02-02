eb673c10aa5d6a42adc2fc65e2f4146a1d9f5612
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-02-02 12:01:59 +0000

    feat(nvim/snacks): install

---
+++ nvim/lua/plugin/snacks.lua
@@ -0,0 +1,12 @@
+---@type LazySpec
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+}
