166decaff6edcc8433a2d6e89e2a8e1a0c8d5fe6
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-02-03 16:36:09 +0000

    feat(nvim/snacks): for vim.ui.input and vim.ui.select instead of dressing

--- nvim/lua/plugin/snacks.lua
+++ nvim/lua/plugin/snacks.lua
@@ -2,11 +2,14 @@
 return {
 	"folke/snacks.nvim",
 	priority = 1000,
-	lazy = false,
+	event = "VeryLazy",
 	---@type snacks.Config
 	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
+		input = {
+			enabled = true,
+		},
+		picker = {
+			ui_select = true,
+		},
 	},
 }
