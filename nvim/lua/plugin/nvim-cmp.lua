9ae581c108806582270eb90180fb6c97f8f6cba8
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-02-03 16:34:16 +0000

    feat(cmp): codecompanion

--- nvim/lua/plugin/nvim-cmp.lua
+++ nvim/lua/plugin/nvim-cmp.lua
@@ -311,6 +311,7 @@ return {
 				{ name = "nvim_lsp" },
 				{ name = "async_path" },
 				{ name = "emoji", insert = true },
+				{ name = "codecompanion" },
 			}, {
 				{ name = "buffer" },
 				{ name = "omni" },
