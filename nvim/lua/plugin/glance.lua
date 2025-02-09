6ca5bde4df355182cc713a4a5220a99c2c978571
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-02-09 16:02:55 +0000

    feat(nvim/glance): fix before_open hook

--- nvim/lua/plugin/glance.lua
+++ nvim/lua/plugin/glance.lua
@@ -8,15 +8,8 @@ return {
 			folds = { folded = false },
 			hooks = {
 				before_open = function(results, open, jump, method)
-					local uri = vim.uri_from_bufnr(0)
 					if #results == 1 then
-						local target_uri = results[1].uri or results[1].targetUri
-
-						if target_uri == uri then
-							jump(results[1])
-						else
-							open(results)
-						end
+						jump(results[1])
 					else
 						open(results)
 					end
