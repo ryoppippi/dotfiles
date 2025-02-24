2dc2f15f1b8e9cc537fb252b162bc2c1572a45f0
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-02-24 10:45:08 +0000

    feat(plugin): add custom textobjects to mini.ai config
    Configure mini.ai plugin with additional textobjects including buffer,
    diagnostic, indent, line, number and custom date pattern matchers to
    enhance text manipulation capabilities.

--- nvim/lua/plugin/mini_ai.lua
+++ nvim/lua/plugin/mini_ai.lua
@@ -3,5 +3,17 @@ return {
 	"echasnovski/mini.ai",
 	branch = "stable",
 	event = "BufReadPost",
-	config = true,
+	opts = function()
+		local gen_ai_spec = require("mini.extra").gen_ai_spec
+		return {
+			custom_textobjects = {
+				e = gen_ai_spec.buffer(),
+				D = gen_ai_spec.diagnostic(),
+				I = gen_ai_spec.indent(),
+				L = gen_ai_spec.line(),
+				N = gen_ai_spec.number(),
+				J = { { "()%d%d%d%d%-%d%d%-%d%d()", "()%d%d%d%d%/%d%d%/%d%d()" } },
+			},
+		}
+	end,
 }
