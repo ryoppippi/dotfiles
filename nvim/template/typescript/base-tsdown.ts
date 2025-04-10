913221270908cd8dc57d61679bb0e3ca44d2e3e6
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-04-10 11:58:54 +0100

    feat(nvim): template for tsdown config

---
+++ nvim/template/typescript/base-tsdown.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsdown";
+
+const config: ReturnType<typeof defineConfig> = defineConfig({
+  outDir: "dist",
+  entry: "src/index.ts{{_cursor_}}",
+  fixedExtension: true,
+  dts: true,
+  clean: true,
+  unused: { level: "error" },
+  publint: true,
+});
+
+export default config;
