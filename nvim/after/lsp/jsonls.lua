5c6897bc5b825cf43fd418b37b92c91cf849cc2b
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-04-24 21:02:15 +0100

    fix(nvim/lspconfig): remove format config

--- nvim/after/lsp/jsonls.lua
+++ nvim/after/lsp/jsonls.lua
@@ -1,8 +1,6 @@
 local lsp_utils = require("plugin.nvim-lspconfig.utils")
-local format_config = lsp_utils.format_config
 
 return {
-	on_attach = format_config(false),
 	filetypes = { "json", "jsonc", "json5" },
 	settings = {
 		json = {
