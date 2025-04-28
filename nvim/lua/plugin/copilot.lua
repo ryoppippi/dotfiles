dfccd82134ecf31bb68bdc6127d514d77a27ae07
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-04-28 13:49:18 +0100

    fix(copilot): disable copilot suggestion hidden management with cmp

--- nvim/lua/plugin/copilot.lua
+++ nvim/lua/plugin/copilot.lua
@@ -38,15 +38,6 @@ return {
 		},
 	},
 	config = function(_, opts)
-		local cmp = require("cmp")
-
-		cmp.event:on("menu_opened", function()
-			vim.b.copilot_suggestion_hidden = true
-		end)
-
-		cmp.event:on("menu_closed", function()
-			vim.b.copilot_suggestion_hidden = false
-		end)
 		vim.defer_fn(function()
 			require("copilot").setup(opts)
 		end, 100)
