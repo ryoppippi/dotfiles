7cd2b38302e8dbafbde5b493d7fdcf1de316362a
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-05-05 01:50:56 +0100

    feat(nvim/log-highlighting): set lazy load event

--- nvim/lua/plugin/vim-log-highlighting.lua
+++ nvim/lua/plugin/vim-log-highlighting.lua
@@ -1,6 +1,13 @@
 return {
 	"mtdl9/vim-log-highlighting",
-	ft = {
-		"log",
-	},
+	event = function()
+		local event = { "BufRead", "BufNewFile" }
+		local filetype = " *{.,_}log"
+
+		local return_events = {}
+		for _, e in ipairs(event) do
+			table.insert(return_events, e .. filetype)
+		end
+		return return_events
+	end,
 }
