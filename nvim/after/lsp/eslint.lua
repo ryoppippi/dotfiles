99ba391e4ce3f4aea69531c2f5737859846eb0ac
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-05-12 16:14:20 +0100

    fix(nvim/lspconfig): disable stylic error in eslint

--- nvim/after/lsp/eslint.lua
+++ nvim/after/lsp/eslint.lua
@@ -1,4 +1,46 @@
+local customizations = {
+	{ rule = "style/*", severity = "off", fixable = true },
+	{ rule = "format/*", severity = "off", fixable = true },
+	{ rule = "*-indent", severity = "off", fixable = true },
+	{ rule = "*-spacing", severity = "off", fixable = true },
+	{ rule = "*-spaces", severity = "off", fixable = true },
+	{ rule = "*-order", severity = "off", fixable = true },
+	{ rule = "*-dangle", severity = "off", fixable = true },
+	{ rule = "*-newline", severity = "off", fixable = true },
+	{ rule = "*quotes", severity = "off", fixable = true },
+	{ rule = "*semi", severity = "off", fixable = true },
+}
+
 ---@type vim.lsp.Config
 return {
 	workspace_required = true,
+	filetypes = {
+		"javascript",
+		"javascriptreact",
+		"javascript.jsx",
+		"typescript",
+		"typescriptreact",
+		"typescript.tsx",
+		"vue",
+		"html",
+		"markdown",
+		"json",
+		"jsonc",
+		"yaml",
+		"toml",
+		"xml",
+		"gql",
+		"graphql",
+		"astro",
+		"svelte",
+		"css",
+		"less",
+		"scss",
+		"pcss",
+		"postcss",
+	},
+	settings = {
+		-- Silent the stylistic rules in you IDE, but still auto fix them
+		rulesCustomizations = customizations,
+	},
 }
