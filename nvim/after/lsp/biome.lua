e50f4a1a0c22e701368df37080511ca6e61f5e9a
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-05-12 16:08:57 +0100

    fix(nvim/lspconfig): biome root_dir

--- nvim/after/lsp/biome.lua
+++ nvim/after/lsp/biome.lua
@@ -1,5 +1,16 @@
 ---@type vim.lsp.Config
 return {
 	workspace_required = true,
-	cmd = { "./node_modules/.bin/biome", "lsp-proxy" },
+	root_dir = function(bufnr, on_dir)
+		local found_dirs = vim.fs.find({
+			"biome.json",
+			"biome.jsonc",
+		}, {
+			upward = true,
+			path = vim.fs.dirname(vim.fs.normalize(vim.api.nvim_buf_get_name(bufnr))),
+		})
+		if #found_dirs > 0 then
+			return on_dir(vim.fs.dirname(found_dirs[1]))
+		end
+	end,
 }
