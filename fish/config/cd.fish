693aad6cb0e904aa00b16aef040f563591976ae0
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-05-20 11:18:34 +0100

    feat(fish): automatically config git sign when pwd is git managed

--- fish/config/cd.fish
+++ fish/config/cd.fish
@@ -2,4 +2,9 @@ function __ls_after_cd__on_variable_pwd --on-variable PWD
     if status --is-interactive
         ls -hlF $PWD
     end
+
+    # if .git directory exists, run git status
+    if test -d $PWD/.git
+        git-sign-config
+    end
 end
