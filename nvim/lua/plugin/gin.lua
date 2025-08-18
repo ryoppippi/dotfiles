9f7aced9348bf97e2d501dd5f42d2fc96ee78cea
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-08-18 16:47:43 +0100

    feat(nvim): add default args to GinBrowse (no browser open, permalink, yank)

--- nvim/lua/plugin/gin.lua
+++ nvim/lua/plugin/gin.lua
@@ -2,13 +2,18 @@
 return {
 	"lambdalisue/vim-gin",
 	event = { "User DenopsReady" },
-	dependencies = { "vim-denops/denops.vim" },
+	dependencies = {
+		{ "vim-denops/denops.vim" },
+		{ "tani/vim-artemis" },
+	},
 	keys = {
 		{ "gp", "Gin push", mode = "ca" },
 		{ "gpl", "Gin pull", mode = "ca" },
 		{ "gc", "Gin commit", mode = "ca" },
 	},
 	config = function(spec)
+		vimx.g.gin_browse_default_args = { "-n", "--permalink" }
+		vimx.g.gin_browse_persistent_args = { "++yank" }
 		require("denops-lazy").load(spec.name)
 	end,
 }
