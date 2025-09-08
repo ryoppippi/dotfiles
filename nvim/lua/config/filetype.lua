b8213009bf09df5d0f682c48e5a193bca5a142bb
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-09-08 17:03:37 +0100

    fix(nvim): add env and dockerfile

--- nvim/lua/config/filetype.lua
+++ nvim/lua/config/filetype.lua
@@ -22,5 +22,16 @@ vim.filetype.add({
 
 		[".*/%.ssh/.*%.conf"] = "sshconfig",
 		[".*/ssh/.*%.conf"] = "sshconfig",
+
+		[".*/env.development"] = "sh",
+		[".*/env.production"] = "sh",
+		[".*/env.staging"] = "sh",
+		[".*/env.local"] = "sh",
+		[".*/%.env%.local"] = "sh",
+		[".*/%.env%.development"] = "sh",
+		[".*/%.env%.production"] = "sh",
+		[".*/%.env%.staging"] = "sh",
+
+		[".*/Dockerfile.*"] = "dockerfile",
 	},
 })
