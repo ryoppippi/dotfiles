eb62ea51cd07145aea04c4e104900f30e2f51d23
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-11-16 22:15:20 +0000

    feat(fish): enable wezterm cli active pane

--- fish/user_functions/fish_right_prompt.fish
+++ fish/user_functions/fish_right_prompt.fish
@@ -7,7 +7,7 @@ function fish_right_prompt
             set duration (echo "$CMD_DURATION 1000" | awk '{printf "%.3fs", $1 / $2}')
             set -f MSG (echo (history | head -1) returned $status after $duration)
             type -q notify-send && notify-send $MSG && return
-            type -q terminal-notifier && terminal-notifier -message $MSG && return
+            type -q terminal-notifier && terminal-notifier -message $MSG -execute "$(which wezterm) cli activate-pane --pane-id $WEZTERM_PANE" -activate com.github.wez.wezterm \  && return
         end
     end
 end
