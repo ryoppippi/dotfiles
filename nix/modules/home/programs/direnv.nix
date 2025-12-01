0ae37087f26841668e4484ccb77e570d54b81ca2
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-12-01 23:18:49 +0000

    refactor(nix): configure option of direnv
    - disable warn timeout
    - disable hide env off
    - minimise debug log
    
    config from nakazye san

--- nix/modules/home/programs/direnv.nix
+++ nix/modules/home/programs/direnv.nix
@@ -3,5 +3,14 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    config = {
+      global = {
+        warn_timeout = "0s";
+        hide_env_diff = true;
+      };
+    };
+    stdlib = ''
+      export DIRENV_LOG_FORMAT=""
+    '';
   };
 }
