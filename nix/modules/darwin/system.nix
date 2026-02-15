8b69ca0d9d31f80ad44f02595b1c7236bdeafd76
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2026-02-15 13:29:22 -0800

    chore(darwin): remove Apple Watch authentication for sudo
    Drop watchIdAuth setting entirely; Touch ID with pam-reattach is
    sufficient for sudo authentication.

--- nix/modules/darwin/system.nix
+++ nix/modules/darwin/system.nix
@@ -18,7 +18,6 @@ in
 
   # Enable Touch ID for sudo (including tmux support via pam-reattach)
   security.pam.services.sudo_local.touchIdAuth = true;
-  security.pam.services.sudo_local.watchIdAuth = true;
   security.pam.services.sudo_local.reattach = true;
 
   system = {
