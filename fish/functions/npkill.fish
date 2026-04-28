89e292b9e683aec4760a93c8d80c89af1d6200df
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2026-04-28 12:24:37 +0100

    fix(fish): improve npkill command performance

--- fish/functions/npkill.fish
+++ fish/functions/npkill.fish
@@ -1,27 +1,27 @@
 function npkill
-    # Check for existence of at least one node_modules directory
-    set -l first (find . -type d -name node_modules -prune -print -quit)
-    if test -z "$first"
+    set -l dirs (fd --hidden --no-ignore --prune --type d '^node_modules$' . 2>/dev/null)
+
+    if test (count $dirs) -eq 0
         echo "No node_modules directories found. Exiting."
         return 0
     end
 
-    # List all node_modules directories with their sizes
-    find . -type d -name node_modules -prune -exec du -sh {} + | sort -rh
+    printf '%s\n' $dirs | xargs -P (getconf _NPROCESSORS_ONLN) -I{} du -sh {} | sort -rh
 
     echo -n "Do you want to move all node_modules directories to /tmp? (y/n) "
     read -l answer
-    if test "$answer" = y
-        set -l timestamp (date +%Y%m%d_%H%M%S)
-        set -l counter 0
-        find . -type d -name node_modules -prune | while read -l dir
-            set counter (math $counter + 1)
-            set -l dest "/tmp/node_modules_$timestamp"_"$counter"
-            mv "$dir" "$dest"
-            echo "Moved: $dir -> $dest"
-        end
-        echo "All node_modules directories have been moved to /tmp."
-    else
+    if test "$answer" != y
         echo "Operation cancelled."
+        return 0
     end
+
+    set -l timestamp (date +%Y%m%d_%H%M%S)
+    set -l counter 0
+    for dir in $dirs
+        set counter (math $counter + 1)
+        set -l dest "/tmp/node_modules_$timestamp"_"$counter"
+        mv "$dir" "$dest"
+        echo "Moved: $dir -> $dest"
+    end
+    echo "All node_modules directories have been moved to /tmp."
 end
