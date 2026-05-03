54582fccdd2cfadf0cce45cc1ca6c35b0c45e250
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2026-05-03 18:58:03 +0300

    feat(fish): add tree preview in fzf ghq

--- fish/functions/__ghq_roots.fish
+++ fish/functions/__ghq_roots.fish
@@ -1,5 +1,5 @@
 function __ghq_roots --description "Navigate to project roots within ghq-managed repositories"
-    set -l selected (ghq list --full-path | roots | fzf --height 40% --reverse)
+    set -l selected (ghq list --full-path | roots | fzf --height 40% --reverse --preview 'eza --tree --level=2 --git-ignore --color=always --icons {} 2>/dev/null; or ls {}')
     if test -n "$selected"
         cd -- "$selected"
         commandline -f repaint
